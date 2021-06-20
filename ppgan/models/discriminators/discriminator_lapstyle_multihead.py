2a6d111d5cf546470da0499aaf20c983a274b8e5
David Gill <davegill@Davids-MBP.home>
2021-06-19 22:06:44 -0400

    kernel ncx3x3

--- ppgan/models/discriminators/discriminator_lapstyle_multihead.py
+++ ppgan/models/discriminators/discriminator_lapstyle_multihead.py
@@ -66,7 +66,7 @@ class LapStyleMultiresDiscriminator(nn.Layer):
             else:
                 net=LapStyleSingleDiscriminator(num_channels=num_channels)
             self.resolutions.append(net)
-        self.pooling = nn.AvgPool3D((num_halvings,1,1),stride=1,padding=0)
+        self.pooling = nn.AvgPool3D((num_halvings,3,3),stride=1,padding=1)
 
     def forward(self, x):
         self.output_resolutions = []

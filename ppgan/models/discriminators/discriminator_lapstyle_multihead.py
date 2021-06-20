bfdc7226bad29b07063e644d7c262cac783edc97
David Gill <davegill@Davids-MBP.home>
2021-06-19 21:27:40 -0400

    conv3d

--- ppgan/models/discriminators/discriminator_lapstyle_multihead.py
+++ ppgan/models/discriminators/discriminator_lapstyle_multihead.py
@@ -73,7 +73,8 @@ class LapStyleMultiresDiscriminator(nn.Layer):
                 self.output_resolutions.append(F.interpolate(self.resolutions[i](x.detach()),scale_factor=2**i))
             else:
                 self.output_resolutions.append(self.resolutions[i](x.detach()))
-        x = paddle.transpose(paddle.to_tensor(self.output_resolutions),(1,2,0,3,4))
+        #x = paddle.transpose(paddle.to_tensor(self.output_resolutions),(1,2,0,3,4))
+        x = paddle.transpose(paddle.to_tensor(self.output_resolutions),(1,0,2,3,4))
         x = self.pooling(x)
         x = x.squeeze(1)
         return x

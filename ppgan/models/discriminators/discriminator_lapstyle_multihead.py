4767bf41c498875faa5c7e7fd34f7c1f1f22b173
David Gill <davegill@Davids-MBP.home>
2021-06-20 00:26:00 -0400

    remove detach, add layerlist

--- ppgan/models/discriminators/discriminator_lapstyle_multihead.py
+++ ppgan/models/discriminators/discriminator_lapstyle_multihead.py
@@ -67,8 +67,7 @@ class LapStyleMultiresDiscriminator(nn.Layer):
                 net=LapStyleSingleDiscriminator(num_channels=num_channels)
             resolutions.append(net)
         self.resolutions = nn.LayerList(resolutions)
-        self.pooling = nn.Sequential(
-            nn.Conv3D(num_halvings,1,1,stride=1,padding=0),
+        self.pooling = nn.Conv3D(num_halvings,1,1,stride=1,padding=0)
 
     def forward(self, x):
         output_resolutions = []

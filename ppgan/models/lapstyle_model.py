db954b483e4a1dc3024121b3b3b1dab890aeaa1d
David Gill <davegill@Davids-MBP.home>
2021-06-25 13:57:14 -0400

    syntax fix

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -558,7 +558,7 @@ class LapStyleDraThumbModel(BaseModel):
         self.loss = self.loss_c * self.content_weight + self.loss_s * self.style_weight +\
                     self.loss_patch * self.content_weight +\
                     self.l_identity1 * 50 + self.l_identity3 *50 +\
-                    self.l_identity2 * 1 + self.l_identity4 *1 self.loss_style_remd * 10 + \
+                    self.l_identity2 * 1 + self.l_identity4 *1 + self.loss_style_remd * 10 + \
                     self.loss_content_relt * 16
         self.loss.backward()
 

096ee844792956a84ba5bc69754320c2d60caaac
David Gill <davegill@Davids-MBP.home>
2021-06-25 18:09:14 -0400

    crop the larger detail for patch out of the same field as the thumbnail

--- ppgan/datasets/lapstyle_dataset.py
+++ ppgan/datasets/lapstyle_dataset.py
@@ -161,27 +161,31 @@ class LapStyleThumbset(Dataset):
         small_edge = min(content_img.width,content_img.height)
         if small_edge==content_img.width:
             intermediate_width = self.load_size
-            final_width = self.crop_size
+            final_width = self.thumb_size
             ratio = content_img.height/content_img.width
             intermediate_height = math.floor(self.load_size*ratio)
-            final_height = math.floor(self.crop_size*ratio)
+            final_height = math.floor(self.thumb_size*ratio)
         else:
+            final_height = self.thumb_size
             intermediate_height = self.load_size
-            final_height = self.crop_size
             ratio = content_img.width/content_img.height
             intermediate_width = math.floor(self.load_size*ratio)
-            final_width = math.floor(self.crop_size*ratio)
+            final_width = math.floor(self.thumb_size*ratio)
+        load_thumb_diff=self.thumb_size/self.load_size
         randx = np.random.randint(0, self.load_size - self.thumb_size)
         randy = np.random.randint(0, self.load_size - self.thumb_size)
         position = [randx, randx+self.thumb_size, randy, randy+self.thumb_size]
         content_img = content_img.resize((intermediate_width, intermediate_height),
                                          Image.BILINEAR)
         content_patches = np.array(content_img)
+
         content_patches = content_patches[randx:randx + self.thumb_size,
                           randy:randy+self.thumb_size] # [8, 3, 256, 256]
-        content_img = content_img.resize((self.thumb_size, self.thumb_size),
+        content_img = content_img.resize((final_width, final_height),
                                          Image.BILINEAR)
         content_img = np.array(content_img)
+        content_img = content_img[max(0,randx-self.thumb_size*load_thumb_diff):randx+self.thumb_size*load_thumb_diff,
+                                    max(0,randy-self.thumb_size*load_thumb_diff):randy+self.thumb_size*load_thumb_diff]
         style_path = random.choice(self.style_paths) if len(self.style_paths)>1 else self.style_paths[0]
         style_img = cv2.imread(style_path)
         style_img = cv2.cvtColor(style_img, cv2.COLOR_BGR2RGB)
@@ -198,7 +202,6 @@ class LapStyleThumbset(Dataset):
         style_img = style_img.resize((intermediate_width, intermediate_height),
                                      Image.BILINEAR)
         style_img = np.array(style_img)
-        content_img = self.transform(content_img)
         style_img = self.transform(style_img)
         content_img = self.img(content_img)
         style_img = self.img(style_img)

44e619516b45f72d82523479a0f1b8f3facef534
David Gill <davegill@Davids-MBP.home>
2021-06-26 16:05:01 -0400

    remove normalization from content loss

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -511,8 +511,7 @@ class LapStyleDraThumbModel(BaseModel):
         #self.loss_c =self.calc_content_loss(self.ttF['r41'],self.cF['r41'])
         for layer in [self.content_layers[-2]]:
             self.loss_c += self.calc_content_loss(self.ttF[layer],
-                                                  self.cF[layer],
-                                                  norm=True)
+                                                  self.cF[layer])
         self.losses['loss_c'] = self.loss_c
         """patch loss"""
         self.loss_patch = 0

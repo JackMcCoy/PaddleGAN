f17b51234a5a42e000902fe491455f68270f3fab
David Gill <davegill@Davids-MBP.home>
2021-06-26 01:09:52 -0400

    adding norm

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -512,7 +512,7 @@ class LapStyleDraThumbModel(BaseModel):
         #              self.calc_content_loss(self.ttF['r51'],self.cF['r51'])
         for layer in self.content_layers:
             self.loss_c += self.calc_content_loss(self.ttF[layer],
-                                                      self.cF[layer],)
+                                                      self.cF[layer],norm=True)
         #                                          norm=True)
         self.losses['loss_c'] = self.loss_c
         """patch loss"""

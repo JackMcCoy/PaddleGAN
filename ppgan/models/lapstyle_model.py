708dbd86db19d8cd71c3fe129a060d283a7755a5
JackMcCoy <fdrdavegill@gmail.com>
2021-06-26 15:36:01 -0400

    all content feature layers

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -509,7 +509,7 @@ class LapStyleDraThumbModel(BaseModel):
         """content loss"""
         self.loss_c = 0
         #self.loss_c =self.calc_content_loss(self.ttF['r41'],self.cF['r41'])
-        for layer in [self.content_layers[-2]]:
+        for layer in self.content_layers:
             self.loss_c += self.calc_content_loss(self.ttF[layer],
                                                   self.cF[layer],)
         #                                          norm=True)

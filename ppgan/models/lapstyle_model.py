473ff22841a31635fdebad6a0e631c33c4f43a88
David Gill <davegill@Davids-MBP.home>
2021-06-26 03:40:13 -0400

    added back 5_1

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -508,19 +508,19 @@ class LapStyleDraThumbModel(BaseModel):
         self.tpF = self.nets['net_enc'](self.stylized_patch)
         """content loss"""
         self.loss_c = 0
-        self.loss_c =self.calc_content_loss(self.ttF['r41'],self.cF['r41'])
-        #for layer in self.content_layers:
-        #    self.loss_c += self.calc_content_loss(self.ttF[layer],
-        #                                          self.cF[layer],
+        #self.loss_c =self.calc_content_loss(self.ttF['r41'],self.cF['r41'])
+        for layer in self.content_layers[3:]:
+            self.loss_c += self.calc_content_loss(self.ttF[layer],
+                                                  self.cF[layer],)
         #                                          norm=True)
         self.losses['loss_c'] = self.loss_c
         """patch loss"""
         self.loss_patch = 0
-        self.loss_patch= self.calc_content_loss(self.tpF['r41'],self.tt_cropF['r41'])#+\
+        #self.loss_patch= self.calc_content_loss(self.tpF['r41'],self.tt_cropF['r41'])#+\
         #                self.calc_content_loss(self.tpF['r51'],self.tt_cropF['r51'])
-        #for layer in self.content_layers[2:]:
-        #    self.loss_patch += self.calc_content_loss(self.tpF[layer],
-        #                                              self.tt_cropF[layer])
+        for layer in self.content_layers[3:]:
+            self.loss_patch += self.calc_content_loss(self.tpF[layer],
+                                                      self.tt_cropF[layer])
         self.losses['loss_patch'] =  self.loss_patch
         """style loss"""
         self.loss_s = 0

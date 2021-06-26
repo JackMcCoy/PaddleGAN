0a5713979200fbd2249e9c2c7b444d001df36f98
David Gill <davegill@Davids-MBP.home>
2021-06-25 23:46:25 -0400

    remove identity 3+4

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -505,11 +505,11 @@ class LapStyleDraThumbModel(BaseModel):
         self.tpF = self.nets['net_enc'](self.stylized_patch)
         """content loss"""
         self.loss_c = 0
-        self.loss_c = self.calc_content_loss(self.ttF['r31'],self.cF['r31'])+self.calc_content_loss(self.ttF['r41'],self.cF['r41'])
+        self.loss_c = self.calc_content_loss(self.ttF['r41'],self.cF['r41'])
         #for layer in self.content_layers:
         #    self.loss_c += self.calc_content_loss(self.ttF[layer],
-                                                      self.cF[layer],
-                                                  norm=True)
+        #                                              self.cF[layer],
+        #                                          norm=True)
         self.losses['loss_c'] = self.loss_c
         """patch loss"""
         self.loss_patch = 0

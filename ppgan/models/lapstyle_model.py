0388cf7e136cf7ac89e89ee8b397bac651d97648
David Gill <davegill@Davids-MBP.home>
2021-06-26 00:56:46 -0400

    no normalize in content loss

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -508,8 +508,8 @@ class LapStyleDraThumbModel(BaseModel):
         self.tpF = self.nets['net_enc'](self.stylized_patch)
         """content loss"""
         self.loss_c = 0
-        self.loss_c = self.calc_content_loss(self.ttF['r41'],self.cF['r41'],norm=True)+\
-                      self.calc_content_loss(self.ttF['r51'],self.cF['r51'],norm=True)
+        self.loss_c = self.calc_content_loss(self.ttF['r41'],self.cF['r41'])+\
+                      self.calc_content_loss(self.ttF['r51'],self.cF['r51'])
         #for layer in self.content_layers:
         #    self.loss_c += self.calc_content_loss(self.ttF[layer],
         #                                              self.cF[layer],

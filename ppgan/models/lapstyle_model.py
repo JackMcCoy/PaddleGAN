2fc113d090611c74223072d9e42f8255ba8509c5
David Gill <davegill@Davids-MBP.home>
2021-06-26 02:46:43 -0400

    return relt, lowered identity a bit to balance

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -568,8 +568,8 @@ class LapStyleDraThumbModel(BaseModel):
 
         self.loss = self.loss_c * self.content_weight + self.loss_s * self.style_weight +\
                     self.loss_patch * self.content_weight +\
-                    self.l_identity1 * 50 + self.l_identity2 * 1 + self.l_identity3 * 50 + self.l_identity2 * 4 +\
-                    self.loss_content_relt * 16 #+ self.loss_style_patch_stroke*10# + self.loss_style_patch_remd*16
+                    self.l_identity1 * 25 + self.l_identity2 * 1 + self.l_identity3 * 25 + self.l_identity2 * 4 +\
+                    self.loss_content_relt * 16 + self.loss_style_patch_stroke*16# + self.loss_style_patch_remd*16
         self.loss.backward()
 
         return self.loss

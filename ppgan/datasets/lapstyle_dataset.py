707c4822c5213e7e9bf40b2367c5aa0ccd6e815c
David Gill <davegill@Davids-MBP.home>
2021-06-27 13:30:44 -0400

    load_size style patch

--- ppgan/datasets/lapstyle_dataset.py
+++ ppgan/datasets/lapstyle_dataset.py
@@ -230,6 +230,7 @@ class LapStyleThumbset(Dataset):
         style_img = self.transform(style_img)
         style_patch = self.transform_patch(style_patch)
         style_patch = style_patch[bottommost*2:topmost*2,leftmost*2:rightmost*2]
+        style_patch = self.img(style_patch)
         content_img = self.img(content_img)
         style_img = self.img(style_img)
         content_patches = self.img(content_patches)

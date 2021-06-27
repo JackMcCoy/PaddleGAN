0c39bbead1186323c90aa6688e7f7f97467f0dac
David Gill <davegill@Davids-MBP.home>
2021-06-27 10:54:08 -0400

    scale up drafting net features

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -689,7 +689,7 @@ class LapStyleRevFirstThumb(BaseModel):
         """content loss"""
         self.loss_c = 0
         for layer in [self.content_layers[-2]]:
-            self.loss_c +=self.calc_content_loss(self.ttF[layer],self.stylized_thumb_feat[layer])
+            self.loss_c +=self.calc_content_loss(self.ttF[layer],F.interpolate(self.stylized_thumb_feat[layer], scale_factor=2, mode='bilinear', align_corners=False))
 
         self.losses['loss_c'] = self.loss_c
 

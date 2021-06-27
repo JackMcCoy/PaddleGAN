e57b166ea93e06cafe224a1f630ebd3518c4175b
David Gill <davegill@Davids-MBP.home>
2021-06-27 13:06:43 -0400

    GAN on patch

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -764,11 +764,20 @@ class LapStyleRevFirstThumb(BaseModel):
 
     def backward_D(self):
         """Calculate GAN loss for the discriminator"""
+        with paddle.no_grad():
+            style_patch = F.interpolate(self.visual_items['si'], scale_factor=2, mode='bilinear', align_corners=False)
+            style_patch_crop = paddle.slice(style_patch,axes=[2,3],starts=[self.position[0],self.position[2]],ends=[self.position[1],self.position[3]])
+            spCrop = self.nets['net_enc'](style_patch_crop)
         pred_fake = self.nets['netD'](self.stylized.detach())
         self.loss_D_fake = self.gan_criterion(pred_fake, False)
+        pred_p_fake = self.nets['netD'](self.p_stylized.detach())
+        self.loss_Dp_fake = self.gan_criterion(pred_p_fake, False)
+
         pred_real = self.nets['netD'](self.pyr_si[2])
         self.loss_D_real = self.gan_criterion(pred_real, True)
-        self.loss_D = (self.loss_D_fake + self.loss_D_real) * 0.5
+        pred_p_real = self.nets['netD'](spCrop)
+        self.loss_Dp_real = self.gan_criterion(pred_p_real, True)
+        self.loss_D = (self.loss_D_fake + self.loss_Dp_fake + self.loss_Dp_real + self.loss_D_real) * 0.5
 
         self.loss_D.backward()
 

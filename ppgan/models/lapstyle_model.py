9f92ff433f6cd29be0d34f4212d366d77ed2cad5
David Gill <davegill@Davids-MBP.home>
2021-06-27 11:42:08 -0400

    if/then conditional on presence of thumbnail features

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -688,7 +688,7 @@ class LapStyleRevFirstThumb(BaseModel):
         """content loss"""
         self.loss_c = 0
         for layer in [self.content_layers[-2]]:
-            self.loss_c +=self.calc_content_loss(self.ttF[layer],self.stylized_thumb_feat[layer])
+            self.loss_c +=self.calc_content_loss(self.ttF[layer],self.stylized_thumb_feat)
 
         self.losses['loss_c'] = self.loss_c
 

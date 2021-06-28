00d17e9de2bb6f7748ed8deac74b34eceeac4672
JackMcCoy <fdrdavegill@gmail.com>
2021-06-28 13:43:11 -0400

    thumb ada throughout

--- ppgan/models/generators/generater_lapstyle.py
+++ ppgan/models/generators/generater_lapstyle.py
@@ -229,19 +229,25 @@ class DecoderThumbNet(nn.Layer):
 
         out = self.upsample(out)
 
+        '''
         if thumb_or_patch=='thumb':
             thumb_ada['r31']=adaptive_instance_normalization(cF['r31'], sF['r31'])
         else:
             thumb_ada['r31']=adaptive_instance_normalization(cpF['r31'], sF['r31'])
         out += thumb_ada['r31']
+        '''
+        out = thumb_adaptive_instance_normalization(cF['r31'], cpF['r31'], sF['r31'], thumb_or_patch=thumb_or_patch)
         out = self.resblock_31(out)
         out = self.convblock_31(out)
 
         out = self.upsample(out)
+        '''
         if thumb_or_patch=='thumb':
             thumb_ada['r21']=adaptive_instance_normalization(cF['r21'], sF['r21'])
         else:
             thumb_ada['r21']=adaptive_instance_normalization(cpF['r21'], sF['r21'])
+        '''
+        out = thumb_adaptive_instance_normalization(cF['r21'], cpF['r21'], sF['r21'], thumb_or_patch=thumb_or_patch)
         out += thumb_ada['r21']
         out = self.convblock_21(out)
         out = self.convblock_22(out)

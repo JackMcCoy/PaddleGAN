379e25007652824c4558a4dc2c2f2ffbf78ce910
JackMcCoy <fdrdavegill@gmail.com>
2021-06-28 15:29:23 -0400

    add loss_c

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -682,11 +682,11 @@ class LapStyleRevFirstThumb(BaseModel):
         self.visual_items['p_stylized_small'] = p_stylized_small
         p_stylized_up = F.interpolate(p_stylized_small, scale_factor=2)
 
-        revnet_input,self.stylized_thumb_feat = paddle.concat(x=[self.pyr_ci[0], stylized_up], axis=1)
-        stylized_rev_lap,_ = self.nets['net_rev'](revnet_input)
+        revnet_input = paddle.concat(x=[self.pyr_ci[0], stylized_up], axis=1)
+        stylized_rev_lap,self.stylized_thumb_feat = self.nets['net_rev'](revnet_input)
         stylized_rev = fold_laplace_pyramid([stylized_rev_lap, stylized_small])
 
-        p_revnet_input,_ = paddle.concat(x=[self.pyr_cp[0], p_stylized_up], axis=1)
+        p_revnet_input = paddle.concat(x=[self.pyr_cp[0], p_stylized_up], axis=1)
         p_stylized_rev_lap,_ = self.nets['net_rev'](p_revnet_input,_)
         p_stylized_rev = fold_laplace_pyramid([p_stylized_rev_lap, p_stylized_small])
 

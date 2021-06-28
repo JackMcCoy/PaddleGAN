9c5982a203ed990a64ce9112b1afe3878e2c2b96
David Gill <davegill@Davids-MBP.home>
2021-06-28 03:00:24 -0400

    identity 1+3 = 30

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -588,8 +588,8 @@ class LapStyleDraThumbModel(BaseModel):
         self.loss = self.loss_c * self.content_weight + self.loss_s * self.style_weight +\
                     self.loss_ps * self.style_weight +\
                     self.loss_patch * self.content_weight * 3 +\
-                    self.l_identity1 * 50 + self.l_identity2 * 1 +\
-                    self.l_identity3 * 50 + self.l_identity4 * 1 +\
+                    self.l_identity1 * 30 + self.l_identity2 * 1 +\
+                    self.l_identity3 * 30 + self.l_identity4 * 1 +\
                     self.loss_content * self.content_weight+ self.loss_p_content * self.content_weight +\
                     self.loss_style_remd * 10 + \
                     self.loss_content_relt * 16 +self.p_loss_style_remd * 10 + self.p_loss_content_relt * 16

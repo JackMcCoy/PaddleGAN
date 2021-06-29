09bcfceb88a0f8912e4cbcc840b65a63401b2954
David Gill <davegill@Davids-MBP.home>
2021-06-29 12:45:52 -0400

    remove patch content loss

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -520,12 +520,6 @@ class LapStyleDraThumbModel(BaseModel):
                                                       norm=True)
         self.losses['loss_content'] = self.loss_content
 
-        self.loss_p_content = 0
-        for layer in self.content_layers:
-            self.loss_p_content += self.calc_content_loss(self.tpF[layer],
-                                                        self.cpF[layer],
-                                                        norm=True)
-        self.losses['loss_p_content'] = self.loss_p_content
         """patch loss"""
         self.loss_patch = 0
         #self.loss_patch= self.calc_content_loss(self.tpF['r41'],self.tt_cropF['r41'])#+\
@@ -594,7 +588,7 @@ class LapStyleDraThumbModel(BaseModel):
                     self.loss_patch * self.content_weight * 1 +\
                     self.l_identity1 * 50 + self.l_identity2 * 1 +\
                     self.l_identity3 * 50 + self.l_identity4 * 1 +\
-                    self.loss_content * self.content_weight+ self.loss_p_content * self.content_weight +\
+                    self.loss_content * self.content_weight+\
                     self.loss_style_remd * 10 +\
                     self.loss_content_relt * 16 + self.p_loss_style_remd * 20 + self.p_loss_content_relt * 32
         self.loss.backward()

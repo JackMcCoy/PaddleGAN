5090d4ca463179221995f32f0aa5547af4d4f10c
JackMcCoy <fdrdavegill@gmail.com>
2021-06-30 17:39:17 -0400

    debugging

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1016,8 +1016,10 @@ class LapStyleRevSecondThumb(BaseModel):
         self.losses['p_loss_style_remd'] = p_loss_style_remd
         self.losses['p_loss_content_relt'] = p_loss_content_relt
 
-        patch_loss = loss_patch * self.content_weight * 40 + \
-                          p_loss_style_remd * 12 + p_loss_content_relt * 16
+        patch_loss = loss_ps * self.style_weight * 2 + \
+                          loss_content_p * self.content_weight + \
+                          loss_patch * self.content_weight * 40 + \
+                          p_loss_style_remd * 12
         patch_loss.backward()
         return patch_loss
 

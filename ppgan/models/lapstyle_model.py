783b8bdf3dcf5037386a3dc974b872452013a38e
David Gill <davegill@Davids-MBP.home>
2021-06-30 07:16:01 -0400

    use the self.losses ref

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -744,8 +744,11 @@ class LapStyleRevFirstThumb(BaseModel):
         pred_fake = self.nets['netD'](self.stylized)
         self.loss_G_GAN = self.gan_criterion(pred_fake, True)
         self.losses['loss_gan_G'] = self.loss_G_GAN
+        pred_fake_p = self.nets['netD'](self.p_stylized)
+        self.loss_Gp_GAN = self.gan_criterion(pred_fake_p, True)
+        self.losses['loss_gan_Gp'] = self.loss_Gp_GAN
 
-        self.loss = self.loss_G_GAN +self.loss_s * self.style_weight +\
+        self.loss = self.loss_G_GAN +self.losses['loss_gan_Gp']+self.loss_s * self.style_weight +\
                     self.loss_content * self.content_weight+\
                     self.loss_style_remd * 10 +\
                     self.loss_content_relt * 16

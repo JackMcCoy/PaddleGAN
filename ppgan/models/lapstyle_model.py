040cd3b91ba5285393cca6365501a976a53945e0
David Gill <davegill@Davids-MBP.home>
2021-06-30 06:48:16 -0400

    no gan loss calc

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -789,16 +789,11 @@ class LapStyleRevFirstThumb(BaseModel):
         self.losses['p_loss_style_remd'] = self.p_loss_style_remd
         self.losses['p_loss_content_relt'] = self.p_loss_content_relt
 
-        """gan loss"""
 
-        pred_fake = self.nets['netD'](self.stylized)
-        self.loss_G_GAN = self.gan_criterion(pred_fake, True)
-        self.losses['loss_gan_G'] = self.loss_G_GAN
-        pred_fake_p = self.nets['netD'](self.p_stylized)
-        self.loss_Gp_GAN = self.gan_criterion(pred_fake_p, True)
-        self.losses['loss_gan_Gp'] = self.loss_Gp_GAN
-
-        self.loss = self.p_loss_style_remd * 25 + self.p_loss_content_relt * 32
+        self.loss = self.loss_ps * self.style_weight *1.5 +\
+                    self.loss_content_p * self.content_weight +\
+                    self.loss_patch * self.content_weight * 40 +\
+                    self.p_loss_style_remd * 25 + self.p_loss_content_relt * 32
         self.loss.backward()
 
         return self.loss

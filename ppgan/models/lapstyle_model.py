c583b8efd11e9925cd3df5e9cf46bcf7137df2c9
JackMcCoy <fdrdavegill@gmail.com>
2021-06-30 17:49:24 -0400

    remove thumb_ada from rev

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1017,6 +1017,7 @@ class LapStyleRevSecondThumb(BaseModel):
         self.losses['p_loss_content_relt'] = p_loss_content_relt
 
         patch_loss = loss_ps * self.style_weight * 2 + \
+                          loss_content_p * self.content_weight + \
                           loss_patch * self.content_weight * 40 + \
                           p_loss_style_remd * 12 + p_loss_content_relt * 16
         patch_loss.backward()

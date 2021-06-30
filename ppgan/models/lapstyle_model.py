b67618d3e8f1f58a01bb3a9e72ba7aa86af9b289
David Gill <davegill@Davids-MBP.home>
2021-06-30 07:28:57 -0400

    no grad call

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -744,12 +744,16 @@ class LapStyleRevFirstThumb(BaseModel):
         pred_fake = self.nets['netD'](self.stylized)
         self.loss_G_GAN = self.gan_criterion(pred_fake, True)
         self.losses['loss_gan_G'] = self.loss_G_GAN
+        with self.nets['netD'].no_grad():
+            pred_fake_p = self.nets['netD'](self.p_stylized)
+            self.loss_Gp_GAN = self.gan_criterion(pred_fake_p, True)
+            self.losses['loss_gan_Gp'] = self.loss_Gp_GAN
 
-        self.loss = self.loss_G_GAN +self.loss_s * self.style_weight +\
+        self.loss = self.loss_G_GAN + self.loss_Gp_GAN+self.loss_s * self.style_weight +\
                     self.loss_content * self.content_weight+\
                     self.loss_style_remd * 10 +\
                     self.loss_content_relt * 16
-        self.loss.backward()
+        self.loss.backward(retain_graph=True)
         optimizer.step()
 
         """patch loss"""

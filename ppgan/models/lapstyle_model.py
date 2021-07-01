5d22340b332f58917d9aac205d6933cd389f46d6
JackMcCoy <fdrdavegill@gmail.com>
2021-06-30 21:16:12 -0400

    visual debugging

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -577,6 +577,7 @@ class LapStyleDraThumbModel(BaseModel):
                 self.loss_ps += self.calc_style_loss(self.tpF[layer], s[layer])
         self.loss_ps = self.loss_ps/4
         self.losses['loss_ps'] = self.loss_ps
+        self.visual_items['stylized_patch'] = reshaped[i,0,:,:,:]
 
         """IDENTITY LOSSES"""
         self.Ipcc,_ = self.nets['net_dec'](self.cpF, self.cpF, self.cpF,'thumb')

777e80292554986f775d68d73da314f299336869
JackMcCoy <fdrdavegill@gmail.com>
2021-06-30 20:30:38 -0400

    no normalization in rev generator

--- ppgan/models/generators/generater_lapstyle.py
+++ ppgan/models/generators/generater_lapstyle.py
@@ -455,8 +455,6 @@ class RevisionNetThumb(nn.Layer):
         out = self.DownBlock(input)
         out = self.resblock(out)
         feats = out.clone()
-        if type(thumbnail) != bool:
-            out +=adaptive_instance_normalization(out,thumbnail)
         out = self.UpBlock(out)
         return out,feats
 

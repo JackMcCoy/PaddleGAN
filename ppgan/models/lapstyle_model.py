39926f459b7dadc8d124d8c9c72565f83b39b2d2
JackMcCoy <fdrdavegill@gmail.com>
2021-07-01 10:48:12 -0400

    same thing with starry

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -601,7 +601,7 @@ class LapStyleDraThumbModel(BaseModel):
         self.losses['p_loss_content_relt'] = self.p_loss_content_relt
         self.losses['p_loss_content_relt'] = self.p_loss_content_relt
 
-        self.loss = self.loss_ps * self.style_weight *5 + self.loss_content_p * self.content_weight +\
+        self.loss = self.loss_ps * self.style_weight *4 + self.loss_content_p * self.content_weight +\
                     self.loss_patch * self.content_weight * 40 +\
                     self.l_identity3 * 55 + self.l_identity4 * 1 +\
                     self.p_loss_style_remd * 20 + self.p_loss_content_relt * 16

c588b12a80246941291f9f7990faf14c6c3815d4
David Gill <davegill@Davids-MBP.home>
2021-07-02 01:46:17 -0400

    fixed style_upsize

--- ppgan/datasets/lapstyle_dataset.py
+++ ppgan/datasets/lapstyle_dataset.py
@@ -213,16 +213,16 @@ class LapStyleThumbset(Dataset):
         small_edge = min(style_img.width,style_img.height)
         if small_edge==style_img.width:
             intermediate_width = self.load_size
-            final_width = self.thumb_size
+            final_width = math.ceil(self.thumb_size*self.style_upsize)
             ratio = style_img.height/style_img.width
             intermediate_height = math.floor(self.load_size*ratio)
             final_height = math.ceil(self.thumb_size*ratio* self.style_upsize)
         else:
-            intermediate_height = math.floor(self.load_size)
-            final_height = self.thumb_size
+            intermediate_height = self.load_size
+            final_height = math.ceil(self.thumb_size * self.style_upsize)
             ratio = style_img.width/style_img.height
             intermediate_width = math.floor(self.load_size* ratio)
-            final_width = self.thumb_size*ratio* self.style_upsize
+            final_width = math.ceil(self.thumb_size*ratio* self.style_upsize)
         style_patch = style_img.resize((intermediate_width, intermediate_height),
                                      Image.BILINEAR)
         style_img = style_patch.resize((final_width,final_height),Image.BILINEAR)

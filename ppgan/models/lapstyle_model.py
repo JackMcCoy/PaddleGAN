9d3559740927b9167977e8edf332952761deaafd
David Gill <davegill@Davids-MBP.home>
2021-07-03 01:13:38 -0400

    patchloss=40

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -611,7 +611,7 @@ class LapStyleDraThumbModel(BaseModel):
         self.losses['p_loss_content_relt'] = self.p_loss_content_relt
 
         self.loss = self.loss_ps * self.style_weight *.9 + self.loss_content_p * self.content_weight +\
-                    self.loss_patch * self.content_weight * 2 +\
+                    self.loss_patch * self.content_weight * 40 +\
                     self.l_identity3 * 50 + self.l_identity4 * 1 +\
                     self.p_loss_style_remd * 18 + self.p_loss_content_relt * 24
         self.loss.backward()

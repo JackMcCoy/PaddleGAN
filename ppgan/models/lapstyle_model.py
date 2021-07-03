739089ddfca126fe6fdd2c026f671306c8d25efb
David Gill <davegill@Davids-MBP.home>
2021-07-03 01:53:59 -0400

    return to sanity

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -805,7 +805,7 @@ class LapStyleRevFirstThumb(BaseModel):
 
         self.patch_loss = self.loss_Gp_GAN +self.loss_ps * self.style_weight *.225 +\
                     self.loss_content_p * self.content_weight +\
-                    self.loss_patch * self.content_weight * 1 +\
+                    self.loss_patch * self.content_weight * 1.5 +\
                     self.p_loss_style_remd * 10 + self.p_loss_content_relt * 16
         self.patch_loss.backward()
 

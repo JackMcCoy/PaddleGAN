e99975469898d438c6ba12655c4787009514a3b6
JackMcCoy <fdrdavegill@gmail.com>
2021-07-06 16:13:27 -0400

    split gP losses

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -783,8 +783,8 @@ class LapStyleRevFirstThumb(BaseModel):
                 self.loss_Dp_real = self.nets['netD_patch'](j.detach())
                 pred_Dp_real += self.nets['netD_patch'](self.loss_Dp_real)
         pred_Dp_real = self.nets['netD_patch'](self.sp.detach())
-        self.loss_Dp_real += self.gan_criterion(pred_Dp_real, True)
-        self.loss_D_patch = (self.loss_Dp_fake + self.loss_Dp_real) * 0.5
+        self.loss_Dp_real = self.gan_criterion(pred_Dp_real, True)
+        self.loss_D_patch = (self.loss_Dp_fake + self.loss_Dp_real/4) * 0.5
 
         self.loss_D_patch.backward()
 

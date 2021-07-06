1c5fe3dd8f788b195a499d1a4e61150bcff8bdf8
David Gill <davegill@Davids-MBP.home>
2021-07-06 00:06:58 -0400

    starry patch, full style and content weight, patch_loss=50

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -599,10 +599,10 @@ class LapStyleDraThumbModel(BaseModel):
         self.losses['p_loss_content_relt'] = self.p_loss_content_relt
 
 
-        self.loss = self.loss_ps * self.style_weight *.55 + self.loss_content_p * self.content_weight*.4 +\
-                    self.loss_patch * self.content_weight * 7 +\
+        self.loss = self.loss_ps * self.style_weight + self.loss_content_p * self.content_weight +\
+                    self.loss_patch * self.content_weight * 50 +\
                     self.l_identity3 * 50 + self.l_identity4 * 1 +\
-                    self.p_loss_style_remd * 18 + self.p_loss_content_relt * 24
+                    self.p_loss_style_remd * 10 + self.p_loss_content_relt * 16
         self.loss.backward()
 
         return self.loss

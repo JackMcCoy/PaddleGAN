df86eaeb3af6337ba8711551d656044ed6501a9e
David Gill <davegill@Davids-MBP.home>
2021-07-07 18:19:56 -0400

    style patch slices

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1460,10 +1460,13 @@ class LapStyleRevFirstPatch(BaseModel):
         self.loss_Dp_fake = self.gan_criterion(pred_p_fake, False)
 
         pred_Dp_real = 0
-
-        self.loss_Dp_real = self.nets['netD_patch'](self.style_patch.detach())
-        pred_Dp_real += self.gan_criterion(self.loss_Dp_real, True)
-        self.loss_D_patch = (self.loss_Dp_fake + pred_Dp_real) * 0.5
+        style_patches = paddle.slice(self.sp,axes=[2,3],starts=[self.position[0],self.position[2]],ends=[self.position[1],self.position[3]])
+        reshaped = paddle.split(style_patches, 2, 2)
+        for i in reshaped:
+            for j in paddle.split(i, 2, 3):
+                self.loss_Dp_real = self.nets['netD_patch'](j)
+                pred_Dp_real += self.gan_criterion(self.loss_Dp_real, True)
+        self.loss_D_patch = (self.loss_Dp_fake + pred_Dp_real/4) * 0.5
 
         self.loss_D_patch.backward()
 

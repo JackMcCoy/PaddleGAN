bcdbc2ccf82397c75f7b5b89d4c6cf7913d7af84
David Gill <davegill@Davids-MBP.home>
2021-07-07 05:39:16 -0400

    config

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1376,13 +1376,12 @@ class LapStyleRevFirstPatch(BaseModel):
         p_stylized_rev_lap,stylized_feats = self.nets['net_rev'](p_revnet_input.detach(),stylized_feats.detach())
         p_stylized_rev = fold_laplace_pyramid([p_stylized_rev_lap, p_stylized_up.detach()])
 
-        patch_origin_size = 256
+        patch_origin_size = 512
         i = random_crop_coords(patch_origin_size)
         input_crop = paddle.slice(p_stylized_rev.detach(),axes=[2,3],starts=[i[0],i[2]],ends=[i[1],i[3]])
         input_crop = F.interpolate(input_crop, scale_factor=2)
-        self.cp_crop = paddle.slice(self.pyr_cp[1],axes=[2,3],starts=[self.position[0],self.position[2]],ends=[self.position[1],self.position[3]])
-
-        self.cp_crop = paddle.slice(self.pyr_cp[1],axes=[2,3],starts=[i[0],i[2]],ends=[i[1]*2,i[3]*2])
+        self.cp_crop = paddle.slice(self.pyr_cp[0],axes=[2,3],starts=[self.position[0],self.position[2]],ends=[self.position[1],self.position[3]])
+        self.cp_crop = paddle.slice(self.pyr_cp[0],axes=[2,3],starts=[i[0],i[2]],ends=[i[1],i[3]])
         p_revnet_input = paddle.concat(x=[self.cp_crop, input_crop], axis=1)
         p_stylized_rev_patch,stylized_feats = self.nets['net_rev_2'](p_revnet_input.detach(),stylized_feats.detach())
         p_stylized_rev_patch = fold_laplace_patch([p_stylized_rev_lap.detach(), p_stylized_up.detach()],p_stylized_rev_patch)

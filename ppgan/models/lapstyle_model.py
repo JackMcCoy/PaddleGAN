d5481350a6ffabe26b5356424de7efcf0017020c
David Gill <davegill@Davids-MBP.home>
2021-07-07 18:23:39 -0400

    remove debug images

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1393,8 +1393,7 @@ class LapStyleRevFirstPatch(BaseModel):
         self.crop_marks = i
         self.style_patch = paddle.slice(self.sp,axes=[2,3],starts=[self.position[0],self.position[2]],ends=[self.position[1],self.position[3]])
         self.style_patch = paddle.slice(self.style_patch,axes=[2,3],starts=[self.crop_marks[0],self.crop_marks[2]],ends=[self.crop_marks[1],self.crop_marks[3]])
-        self.visual_items['input_crop'] = self.input_crop
-        self.visual_items['content_patch']=self.content_patch
+
 
 
     def backward_G(self, optimizer):

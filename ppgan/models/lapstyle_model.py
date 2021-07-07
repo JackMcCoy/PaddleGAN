9e851ad90c449fc6617c619631d9c59cb6927058
David Gill <davegill@Davids-MBP.home>
2021-07-07 16:20:22 -0400

    fold pyramid

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1379,8 +1379,7 @@ class LapStyleRevFirstPatch(BaseModel):
         i = random_crop_coords(patch_origin_size)
 
         self.input_crop = paddle.slice(stylized_up.detach(),axes=[2,3],starts=[i[0],i[2]],ends=[i[1],i[3]])
-        cp_crop = paddle.slice(cp_crop,axes=[2,3],starts=[self.half_position[0],self.half_position[2]],ends=[self.half_position[1],self.half_position[3]])
-        cp_crop = paddle.slice(cp_crop,axes=[2,3],starts=[i[0],i[2]],ends=[i[1],i[3]])
+        cp_crop = paddle.slice(self.pyr_cp[0],axes=[2,3],starts=[i[0],i[2]],ends=[i[1],i[3]])
         p_revnet_input = paddle.concat(x=[cp_crop, self.input_crop], axis=1)
         p_stylized_rev_patch,_ = self.nets['net_rev_2'](p_revnet_input)
         p_stylized_rev_patch = fold_laplace_pyramid([p_stylized_rev_patch, self.input_crop])

9fb20e6d69c1b4e34eceb9c7d244a2206f465884
David Gill <davegill@Davids-MBP.home>
2021-07-08 04:36:44 -0400

    GAN * 2.5

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1445,7 +1445,7 @@ class LapStyleRevFirstPatch(BaseModel):
         self.losses['loss_gan_Gp'] = self.loss_Gp_GAN
 
 
-        self.loss = self.loss_Gp_GAN +self.loss_ps * self.style_weight +\
+        self.loss = self.loss_Gp_GAN*2.5 +self.loss_ps * self.style_weight +\
                     self.loss_content_p * self.content_weight +\
                     self.loss_patch * self.content_weight * 50 +\
                     self.p_loss_style_remd * 25 + self.p_loss_content_relt * 18

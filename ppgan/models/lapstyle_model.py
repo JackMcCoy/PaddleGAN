f902e3c221bc3be1ec3225b1ca00e0bfa26077c0
David Gill <davegill@Davids-MBP.home>
2021-07-08 20:04:30 -0400

    16 channels

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1448,7 +1448,7 @@ class LapStyleRevFirstPatch(BaseModel):
         self.loss = self.loss_Gp_GAN +self.loss_ps * self.style_weight +\
                     self.loss_content_p * self.content_weight +\
                     self.loss_patch * self.content_weight +\
-                    self.p_loss_style_remd * 16 + self.p_loss_content_relt * 10
+                    self.p_loss_style_remd * 16 +   self.p_loss_content_relt * 10
         self.loss.backward()
 
         return self.loss

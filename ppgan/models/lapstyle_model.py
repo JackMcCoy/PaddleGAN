45fbfd4f5210497d753836c04bac8f363171050e
David Gill <davegill@Davids-MBP.home>
2021-07-13 17:01:04 -0400

    debug

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1060,13 +1060,7 @@ class LapStyleRevSecondThumb(BaseModel):
 
 def crop_upsized(stylized_up,positions,orig_size):
     ratio = stylized_up.shape[-1]/orig_size
-    for i in range(stylized_up.shape[0]+1):
-        print(positions[0][i])
-        print(positions[2][i])
-        print(positions[1][i])
-        print(positions[3][i])
-        stylized_up[i]=stylized_up[i,:,math.floor(positions[0][i]*ratio[0]):math.floor(positions[2][i]*ratio[0]),\
-                       math.floor(positions[1][i]*ratio[0]):math.floor(positions[3][i]*ratio[0])]
+    stylized_up=paddle.slice(stylized_up,axes=[2,3],starts=[positions[0]*ratio,positions[1]*ratio],ends=[positions[2]*ratio,positions[3]*ratio)])
     return stylized_up
 
 

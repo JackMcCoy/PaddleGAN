5f1a1ba262a26046ff40d8286bf01ffc58d687e6
David Gill <davegill@Davids-MBP.home>
2021-07-13 17:47:01 -0400

    debug

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1063,6 +1063,9 @@ def crop_upsized(stylized_up,positions,orig_size):
     print(orig_size)
     print(ratio)
     print(positions[0]/ratio)
+    print(positions[1]/ratio)
+    print(positions[2]/ratio)
+    print(positions[3]/ratio)
     stylized_up=paddle.slice(stylized_up,axes=[2,3],starts=[positions[0]/ratio,positions[1]/ratio],ends=[positions[2]/ratio,positions[3]/ratio])
     return stylized_up
 

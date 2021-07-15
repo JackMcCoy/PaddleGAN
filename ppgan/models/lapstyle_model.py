34bcb92e97edbc59e0a4aafeef2ed33ee3e8914a
David Gill <davegill@Davids-MBP.home>
2021-07-15 17:38:46 -0400

    loss_patch=15, style remd=10, content=16

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1239,8 +1239,8 @@ class LapStyleRevSecondPatch(BaseModel):
 
         self.loss = self.loss_Gp_GAN +self.loss_ps/4 * self.style_weight +\
                     self.loss_content_p * self.content_weight +\
-                    self.loss_patch * self.content_weight * 10 +\
-                    self.p_loss_style_remd/4 * 22 + self.p_loss_content_relt * 22
+                    self.loss_patch * self.content_weight * 15 +\
+                    self.p_loss_style_remd/4 * 10 + self.p_loss_content_relt * 16
         self.loss.backward()
 
         return self.loss
@@ -1299,8 +1299,8 @@ class LapStyleRevSecondPatch(BaseModel):
 
         loss_patch = loss_Gp_GAN +loss_ps/4 * self.style_weight +\
                     loss_content_p * self.content_weight +\
-                    loss_patch * self.content_weight * 10 +\
-                    p_loss_style_remd/4 * 22 + p_loss_content_relt * 22
+                    loss_patch * self.content_weight * 15 +\
+                    p_loss_style_remd/4 * 10 + p_loss_content_relt * 16
         loss_patch.backward()
 
         return loss_patch

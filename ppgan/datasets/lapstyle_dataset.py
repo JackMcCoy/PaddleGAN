78939b01e1867d74c19cfcd1422ea4c0557d97f4
David Gill <davegill@Davids-MBP.home>
2021-07-21 01:59:53 -0400

    fix path

--- ppgan/datasets/lapstyle_dataset.py
+++ ppgan/datasets/lapstyle_dataset.py
@@ -309,6 +309,7 @@ class MultiPatchSet(Dataset):
         position_stack = []
         size_stack = []
         path = self.paths[index]
+        print(path)
         content_img = cv2.imread(os.path.join(self.content_root, path))
         try:
             if content_img.ndim == 2:

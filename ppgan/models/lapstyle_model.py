b75b579ae62df7bdfd6209be18c4e1104863d03b
David Gill <davegill@Davids-MBP.home>
2021-07-20 20:33:32 -0400

    new scaling

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1299,7 +1299,7 @@ class LapStyleRevSecondPatch(BaseModel):
                 stylized_feats_2 = self.nets['net_rev_2'].DownBlock(revnet_input.detach())
                 stylized_feats_2 = self.nets['net_rev_2'].resblock(stylized_feats_2)
                 lap_2 = paddle.slice(self.laplacians[2],axes=[2,3],starts=[self.outer_loop[0]*4+i*2,self.outer_loop[1]*4+j*2],
-                                   ends=[self.outer_loop[0]*2+i+in_size_x,self.outer_loop[1]*4+2+j*2+in_size_y])
+                                   ends=[self.outer_loop[0]*4+i*2+in_size_x,self.outer_loop[1]*4+j*2+in_size_y])
                 print('lap_2.shape[-2]='+str(lap_2.shape[-2]))
                 print('lap_2.shape[-1]='+str(lap_2.shape[-1]))
                 if lap_2.shape[-2]!=in_size_x or lap_2.shape[-1]!=in_size_y:

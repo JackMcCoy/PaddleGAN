48208e268c31a1ff7e102a87c23262e236d5ddb5
David Gill <davegill@Davids-MBP.home>
2021-07-21 15:08:36 -0400

    regular style rev 4; reg gan

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1494,7 +1494,7 @@ class LapStyleRevSecondPatch(BaseModel):
         self.losses['loss_gan_Gp2'] = loss_Gp_GAN
 
 
-        loss_patch = loss_Gp_GAN * 2+loss_ps/4 * self.style_weight*1.5 +\
+        loss_patch = loss_Gp_GAN * 1.5+loss_ps/4 * self.style_weight +\
                     loss_content_p * self.content_weight +\
                     loss_patch * self.content_weight *20 +\
                     p_loss_style_remd/4 * 16 + p_loss_content_relt * 26

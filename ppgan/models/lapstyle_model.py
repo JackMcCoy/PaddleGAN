b41f6c8210168637f565ae9cedff7d3d5d184da7
David Gill <davegill@Davids-MBP.home>
2021-07-22 16:08:51 -0400

    16px gutters

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1240,10 +1240,10 @@ class LapStyleRevSecondPatch(BaseModel):
                     else:
                         tiles_2[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1],:] = image
                     '''
-                    x_mod_1=12
-                    x_mod_2=12
-                    y_mod_1=12
-                    y_mod_2=12
+                    x_mod_1=16
+                    x_mod_2=16
+                    y_mod_1=16
+                    y_mod_2=16
                     if b[0]==0:
                         x_mod_1=0
                     if b[1]==0:

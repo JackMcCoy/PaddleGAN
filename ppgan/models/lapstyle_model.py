705f2cbc1e04c78692524a050a7742d3d978479f
David Gill <davegill@Davids-MBP.home>
2021-07-21 23:14:09 -0400

    center box window

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1198,7 +1198,8 @@ class LapStyleRevSecondPatch(BaseModel):
                     print(str(i)+', '+str(j))
                     self.outer_loop=(i,j)
                     self.positions=[[i,j,i+self.in_size_x,j+self.in_size_y]]#
-                    if self.positions[0]+self.positions[2]>self.stylized_slice.shape[-2] or self.positions[1]+self.positions[3]>self.stylized_slize.shape[-1]# !
+                    if self.positions[0]+self.positions[2]>self.stylized_slice.shape[-2] or self.positions[1]+self.positions[3]>self.stylized_slize.shape[-1]:
+                        continue
                     self.test_forward(self.stylized_slice,self.stylized_feats)
             style_paths = [i for i in os.listdir(os.path.join(self.output_dir, 'visual_test'))]
             style_paths = [i for i in style_paths if '_' in i]

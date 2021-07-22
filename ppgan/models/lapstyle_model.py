e85b26ffed49defe8a1b724f650cfdf7b2a90b9a
David Gill <davegill@Davids-MBP.home>
2021-07-22 02:46:02 -0400

    no margins

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1192,8 +1192,8 @@ class LapStyleRevSecondPatch(BaseModel):
                 self.in_size_y = math.floor(size_y / 2)
                 move_y = adjust(size_y, self.in_size_y)
                 ranges_y=list(range(0,size_y,move_y))
-            ranges_x = ranges_x + [i+100 for i in ranges_x[:-1]]
-            ranges_y = ranges_y + [i+100 for i in ranges_y[:-1]]
+            ranges_x = ranges_x + [i+math.floor(self.in_size_x/16) for i in ranges_x[:-1]]
+            ranges_y = ranges_y + [i+math.floor(self.in_size_y/16) for i in ranges_y[:-1]]
             print('ranges x: '+str(ranges_x))
             print('ranges y: '+str(ranges_y))
             for i in ranges_x:

feb3c0c0323794c7d5f297a2479ddb172a0bb364
David Gill <davegill@Davids-MBP.home>
2021-07-23 18:19:50 -0400

    patch style/content losses draft thub

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -521,7 +521,7 @@ class LapStyleDraThumbModel(BaseModel):
         with paddle.no_grad():
             g_t_thumb_up = F.interpolate(self.stylized_thumb, scale_factor=2, mode='bilinear', align_corners=False)
             g_t_thumb_crop = paddle.slice(g_t_thumb_up,axes=[2,3],starts=[self.position[1].astype('int32'),self.position[0].astype('int32')],\
-                             ends=[self.position[3].astype('int32'),self.position[2].astype('int32')])
+                             ends=[self.position[2].astype('int32'),self.position[3].astype('int32')])
             self.tt_cropF = self.nets['net_enc'](g_t_thumb_crop)
             self.spCrop = self.nets['net_enc'](self.sp)
         self.ttF = self.nets['net_enc'](self.stylized_thumb)

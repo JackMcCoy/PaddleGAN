9c8afebbd93145dbf1b6934173e22ba4a1fc1e66
JackMcCoy <fdrdavegill@gmail.com>
2021-07-23 02:13:58 -0400

    multiply by weight of number of visits

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1225,17 +1225,17 @@ class LapStyleRevSecondPatch(BaseModel):
                     k = np.maximum(k,empty)
                     w = weights[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1]]+k
                     tiles_1[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1],0] = image[:,:,0]*k*(k/w) +\
-                                                                                   (tiles_1[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1],0]*(1-k+(1-(k/w))))
+                                                                                   (tiles_1[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1],0]*(1-k)*(1-(k/w)))
                     tiles_1[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1], 1] = image[:, :, 1] *k*(k/w) + \
                                                                                          (tiles_1[
                                                                                           b[0]:b[0] + image.shape[0],
                                                                                           b[1]:b[1] + image.shape[1],
-                                                                                          1] *(1-k+(1-(k/w))))
+                                                                                          1] *(1-k)*(1-(k/w)))
                     tiles_1[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1], 2] = image[:, :,2] *k*(k/w) + \
                                                                                          (tiles_1[
                                                                                           b[0]:b[0] + image.shape[0],
                                                                                           b[1]:b[1] + image.shape[1],
-                                                                                          2] *(1-k+(1-(k/w))))
+                                                                                          2] *(1-k)*(1-(k/w)))
                     not_visited[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1]]=1
                     weights[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1]] = weights[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1]]+k
             for a,b in zip([tiles_1],['tiled1']):

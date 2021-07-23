7c6bbdf718abbc14940b1f4650471377bca42304
JackMcCoy <fdrdavegill@gmail.com>
2021-07-23 02:04:30 -0400

    multiply by weight of number of visits

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1224,16 +1224,14 @@ class LapStyleRevSecondPatch(BaseModel):
                     k = kernel.copy()
                     k = np.maximum(k,empty)
                     w = weights[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1]]+k
-                    tiles_1[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1],0] = image[:,:,0]*(k-((w-k)/w)) +\
+                    tiles_1[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1],0] = image[:,:,0]*(k/w) +\
                                                                                    (tiles_1[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1],0]*(1-k+((w-k)/w)))
-                    tiles_1[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1], 1] = image[:, :, 1] * (
-                                k - ((w - k) / w)) + \
+                    tiles_1[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1], 1] = image[:, :, 1] *(k/w) + \
                                                                                          (tiles_1[
                                                                                           b[0]:b[0] + image.shape[0],
                                                                                           b[1]:b[1] + image.shape[1],
                                                                                           1] * (1 - k + ((w - k) / w)))
-                    tiles_1[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1], 2] = image[:, :,2] * (
-                                k - ((w - k) / w)) + \
+                    tiles_1[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1], 2] = image[:, :,2] *(k/w) + \
                                                                                          (tiles_1[
                                                                                           b[0]:b[0] + image.shape[0],
                                                                                           b[1]:b[1] + image.shape[1],

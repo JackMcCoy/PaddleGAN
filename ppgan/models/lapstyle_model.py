a4b9b586b0826f7ded75ff1e3d646a978000660b
JackMcCoy <fdrdavegill@gmail.com>
2021-07-23 01:48:36 -0400

    multiply by weight of number of visits

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1223,15 +1223,15 @@ class LapStyleRevSecondPatch(BaseModel):
                     empty = np.isnan(not_visited[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1]])
                     k = kernel.copy()
                     k = np.maximum(k,empty)
-                    w = weights[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1]]
-                    tiles_1[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1],0] = image[:,:,0]*(k*(1/w+1)) +\
-                                (tiles_1[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1],0]*((1-k)/(w/w+1))
-                    tiles_1[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1], 1] = image[:, :, 1] * (k*(1/w+1)) + (
-                                tiles_1[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1], 1] *((1-k)/(w/w+1))
-                    tiles_1[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1], 2] = image[:, :, 2] * (k*(1/w+1)) + (
-                                tiles_1[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1], 2] *((1-k)/(w/w+1))
+                    w = weights[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1],0]=image[:,:] +k
+                    tiles_1[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1],0] = image[:,:,0]*k*(k/w) +\
+                                                                                   (tiles_1[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1],0]*(1-k)*((w-k/w)))
+                    tiles_1[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1], 1] = image[:, :, 1] * k*(k/w) + (
+                                tiles_1[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1], 1] * (1 - k)*(w-k/w))
+                    tiles_1[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1], 2] = image[:, :, 2] * k*(k/w) + (
+                                tiles_1[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1], 2] * (1 - k)*(w-k/w))
                     not_visited[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1]]=1
-                    weights[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1]] = weights[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1]]+(k-(1/w))
+                    weights[b[0]:b[0] + image.shape[0], b[1]:b[1] + image.shape[1], 0] = image[:, :] = weights[b[0]:b[0]+image.shape[0],b[1]:b[1]+image.shape[1],0]=image[:,:]+k
             for a,b in zip([tiles_1],['tiled1']):
                 im = Image.fromarray(a,'RGB')
                 label = self.path[0]+' '+b

fe0a3adb8b221bfb992d9a398e5d2e224c013c54
JackMcCoy <fdrdavegill@gmail.com>
2021-07-22 22:46:04 -0400

    fix up set designation

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1174,17 +1174,19 @@ class LapStyleRevSecondPatch(BaseModel):
             size_x = self.stylized_slice.shape[-2]
             self.in_size_x = math.floor(size_x / 2)
             move_x = adjust(size_x, self.in_size_x)
-            ranges_x=list(range(0,size_x,self.in_size_x))
+            ranges_x=list(range(0,size_x-self.in_size_x+1,move_x))
             size_y = self.stylized_slice.shape[-1]
             self.in_size_y = math.floor(size_y / 2)
             move_y = adjust(size_y, self.in_size_y)
-            ranges_y = list(range(0,size_y+1,self.in_size_y))
+            ranges_y = list(range(0,size_y-self.in_size_y+1,move_y))
             orig_len_y = len(ranges_y)
             orig_len_x = len(ranges_x)
             curr_last_x=ranges_x[-1]
             curr_last_y=ranges_y[-1]
             ranges_x = ranges_x + [i+math.floor(self.in_size_x/2) for i in ranges_x[:-1]]
             ranges_y = ranges_y + [i+math.floor(self.in_size_y/2) for i in ranges_y[:-1]]
+            ranges_x.append(curr_last_x-math.floor(self.in_size_x/2))
+            ranges_y.append(curr_last_y-math.floor(self.in_size_y/2))
             self.second_set='a'
             for idx,i in enumerate(ranges_x):
                 for idx2,j in enumerate(ranges_y):

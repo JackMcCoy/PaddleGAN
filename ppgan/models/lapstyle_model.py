9dea29a2800aa323133a5d9d3b3b9bb3a75b44b3
JackMcCoy <fdrdavegill@gmail.com>
2021-07-24 17:20:18 -0400

    patch_loss=.1, increased style and content remds

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -766,9 +766,9 @@ class LapStyleRevFirstThumb(BaseModel):
 
         self.loss = self.loss_G_GAN + self.loss_s *self.style_weight +\
                     self.loss_content * self.content_weight+\
-                    self.loss_patch*self.content_weight*.75+\
-                    self.loss_style_remd * 10 +\
-                    self.loss_content_relt * 16
+                    self.loss_patch*self.content_weight*.1+\
+                    self.loss_style_remd * 20 +\
+                    self.loss_content_relt * 26
         self.loss.backward()
         optimizer.step()
 
@@ -813,8 +813,8 @@ class LapStyleRevFirstThumb(BaseModel):
         self.loss = self.loss_Gp_GAN+self.loss_ps  * self.style_weight*1.5 +\
                           self.loss_content_p * self.content_weight +\
                     self.loss_content_p * self.content_weight +\
-                    self.loss_patch * self.content_weight*.75 +\
-                    self.p_loss_style_remd * 10 + self.p_loss_content_relt * 26
+                    self.loss_patch * self.content_weight*.1 +\
+                    self.p_loss_style_remd * 20 + self.p_loss_content_relt * 26
         self.loss.backward()
 
         return self.loss

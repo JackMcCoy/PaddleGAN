0fa4c19d8b695218cf3460aa9266ab71683836cc
JackMcCoy <fdrdavegill@gmail.com>
2021-07-24 17:35:12 -0400

    style=5, increase gan to 1.5 and patch to .5

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -764,9 +764,9 @@ class LapStyleRevFirstThumb(BaseModel):
         self.loss_G_GAN = self.gan_criterion(pred_fake, True)
         self.losses['loss_gan_G'] = self.loss_G_GAN
 
-        self.loss = self.loss_G_GAN + self.loss_s *self.style_weight +\
+        self.loss = self.loss_G_GAN*1.5 + self.loss_s *self.style_weight +\
                     self.loss_content * self.content_weight+\
-                    self.loss_patch*self.content_weight*.1+\
+                    self.loss_patch*self.content_weight*.5+\
                     self.loss_style_remd * 20 +\
                     self.loss_content_relt * 26
         self.loss.backward()
@@ -810,10 +810,10 @@ class LapStyleRevFirstThumb(BaseModel):
         self.losses['loss_gan_Gp'] = self.loss_Gp_GAN
 
 
-        self.loss = self.loss_Gp_GAN+self.loss_ps  * self.style_weight*1.5 +\
+        self.loss = self.loss_Gp_GAN+self.loss_ps *1.5 * self.style_weight*1.5 +\
                           self.loss_content_p * self.content_weight +\
                     self.loss_content_p * self.content_weight +\
-                    self.loss_patch * self.content_weight*.1 +\
+                    self.loss_patch * self.content_weight*.5 +\
                     self.p_loss_style_remd * 20 + self.p_loss_content_relt * 26
         self.loss.backward()
 

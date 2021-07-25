fcc4b53726fd18c797cc5ecbe8d7d36a4624050c
JackMcCoy <fdrdavegill@gmail.com>
2021-07-25 16:43:19 -0400

    patch_loss rev2 =30

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -770,7 +770,7 @@ class LapStyleRevFirstThumb(BaseModel):
         self.loss = self.loss_Gp_GAN +self.loss_ps * self.style_weight*2 +\
                           self.loss_content_p * self.content_weight +\
                     self.loss_content_p * self.content_weight +\
-                    self.loss_patch * self.content_weight * 50 +\
+                    self.loss_patch * self.content_weight * 30 +\
                     self.p_loss_style_remd * 26 + self.p_loss_content_relt * 26
         self.loss.backward()
 

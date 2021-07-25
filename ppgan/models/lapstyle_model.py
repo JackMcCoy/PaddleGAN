571fddf698c2c98fdd8ea1c2d9f26c0c3949ffb0
David Gill <davegill@Davids-MBP.home>
2021-07-25 04:34:13 -0400

    fix typo/120 style remd

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1507,9 +1507,9 @@ class LapStyleRevSecondPatch(BaseModel):
         self.losses['loss_gan_Gp2'] = loss_Gp_GAN
 
 
-        loss_patch = loss_Gp_GAN +loss_ps/4 * self.style_weight*1.25 +\
+        loss_patch = loss_Gp_GAN*1.25 +loss_ps/4 * self.style_weight*1.25 +\
                     loss_content_p * self.content_weight +\
-                    p_loss_style_remd/4 * 120 + p_loss_content_relt * 26
+                    p_loss_style_remd/4 * 26 + p_loss_content_relt * 26
                      #loss_patch * self.content_weight *20 +\
         loss_patch.backward()
 

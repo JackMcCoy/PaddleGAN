2f853377a4dbccbe171e595e3869e59f11672860
David Gill <davegill@Davids-MBP.home>
2021-07-25 22:15:44 -0400

    increase style remd

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -1454,7 +1454,7 @@ class LapStyleRevSecondPatch(BaseModel):
         self.loss = self.loss_Gp_GAN +self.loss_ps/4 * self.style_weight +\
                     self.loss_content_p * self.content_weight +\
                     self.loss_patch * self.content_weight +\
-                    self.p_loss_style_remd/4 * 18 + self.p_loss_content_relt * 26
+                    self.p_loss_style_remd/4 * 18 + self.p_loss_content_relt * 18
         self.loss.backward()
 
         return self.loss

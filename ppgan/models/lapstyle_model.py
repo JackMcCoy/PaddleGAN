e2941cff0808981500fdd113d3e2c473ab5dc282
JackMcCoy <fdrdavegill@gmail.com>
2021-07-27 20:36:51 -0400

    style patch alpha = .1

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -755,12 +755,11 @@ class LapStyleRevFirstThumb(BaseModel):
         self.losses['loss_ps'] = self.loss_ps
 
         self.loss_psp = 0
-        '''
         for layer in self.content_layers:
             self.loss_psp += self.calc_style_loss(self.tpF[layer],
                                                  self.spF[layer])
         self.losses['loss_psp'] = self.loss_psp
-        '''
+
         style_mix_loss = self.loss_psp * self.style_patch_alpha + (1-self.style_patch_alpha)*self.loss_ps
 
         self.p_loss_style_remd = self.calc_style_emd_loss(

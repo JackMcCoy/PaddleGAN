530f05186f60f653c361fe96cb7d24f97c4f3511
David Gill <davegill@Davids-MBP.home>
2021-07-28 20:17:31 -0400

    style_patch instead of content_patch

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -643,8 +643,6 @@ class LapStyleRevFirstThumb(BaseModel):
         self.si = paddle.to_tensor(input['si'])
         self.cp = input['content']
         self.sp = input['style']
-        self.cp = F.interpolate(self.cp, scale_factor=.5)
-        self.sp = F.interpolate(self.sp, scale_factor=.5)
         self.visual_items['cp'] = self.cp
 
         self.pyr_ci = make_laplace_pyramid(self.ci, 1)

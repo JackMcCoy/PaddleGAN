53364a4a7d7b862e0e4c604d9bada9ff0a19ee82
David Gill <davegill@Davids-MBP.home>
2021-08-02 09:44:13 -0400

    debug

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -60,7 +60,6 @@ def gaussian_filter(sigma):
     gaussian_filter = paddle.nn.Conv2D(3, 3,kernel_size,
                                 groups=3, bias_attr=False,
                                 padding=1, padding_mode='reflect')
-    print(gaussian_filter.value())
     print(gaussian_filter.weight.value())
     gaussian_filter.weight=gaussian_filter.weight.set_value(gaussian_kernel)
 

053923e058665954bce649c06021f0c60bd0221d
David Gill <davegill@Davids-MBP.home>
2021-08-02 16:31:58 -0400

    split morph filter

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -42,7 +42,7 @@ def xdog(im, g, g2,morph_conv,gamma=0.94, phi=50, eps=-0.1, k=1.6):
     for i in range(im.shape[1]):
         imdiff[:,i,:,:] -= imdiff[:,i,:,:].min(axis=0)
         imdiff[:,i,:,:] /= imdiff[:,i,:,:].max(axis=0)
-    morphed = morph_conv(imdiff)
+    morphed = (imdiff*.4)+(morph_conv(imdiff)*.6)
     for i in range(im.shape[1]):
         mean = imdiff[:,i,:,:].mean(axis=0)
         morphed[:,i,:,:]= (morphed[:,i,:,:] > mean).astype('float32') + 0*(morphed[:,i,:,:]<=mean).astype('float32')

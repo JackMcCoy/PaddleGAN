b2f1aefb10d3d6c60f87b6f9c9226a19e352fab6
David Gill <davegill@Davids-MBP.home>
2021-08-02 16:00:29 -0400

    std gaussian2=1.6

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -181,7 +181,7 @@ class LapStyleDraXDOG(BaseModel):
                                 padding=4, padding_mode='reflect')
         self.gaussian_filter_2 = paddle.nn.Conv2D(1, 1,10,
                                 groups=1, bias_attr=False,
-                                weight_attr=paddle.ParamAttr(initializer=paddle.nn.initializer.Normal(mean=0,std=1.8),trainable=False),
+                                weight_attr=paddle.ParamAttr(initializer=paddle.nn.initializer.Normal(mean=0,std=1.6),trainable=False),
                                 padding=4, padding_mode='reflect')
         self.morph_conv = paddle.nn.Conv2D(3,3,5,padding=2,groups=3,padding_mode='reflect',bias_attr=False)
         self.set_requires_grad([self.morph_conv], False)

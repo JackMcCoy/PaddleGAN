f6d71a3c27b4d8926083e9ec8e5a4ea5b4ceba5a
David Gill <davegill@Davids-MBP.home>
2021-08-02 22:14:43 -0400

    gamma=96

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -31,7 +31,7 @@ from ..utils.filesystem import makedirs, save, load
 
 
 
-def xdog(im, g, g2,morph_conv,gamma=0.98, phi=200, eps=-0.5, k=1.6):
+def xdog(im, g, g2,morph_conv,gamma=0.96, phi=200, eps=-0.5, k=1.6):
     # Source : https://github.com/CemalUnal/XDoG-Filter
     # Reference : XDoG: An eXtended difference-of-Gaussians compendium including advanced image stylization
     # Link : http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.365.151&rep=rep1&type=pdf

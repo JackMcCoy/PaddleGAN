c67deb5c5f05da82deca7e012847988843e716d0
David Gill <davegill@Davids-MBP.home>
2021-08-03 03:23:18 -0400

    debug gaussian

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -38,8 +38,7 @@ def xdog(im, g, g2,morph_conv,gamma=1.5, phi=200, eps=-.5, k=1.6):
     #imf1 = paddle.concat(x=[g(paddle.unsqueeze(im[:,0,:,:].detach(),axis=1)),g(paddle.unsqueeze(im[:,1,:,:].detach(),axis=1)),g(paddle.unsqueeze(im[:,2,:,:].detach(),axis=1))],axis=1)
     imf2=paddle.zeros_like(im)
     imf2.stop_gradient=True
-    for i in range(im.shape[1]):
-        imf2[:,i,:,:]=g2(paddle.unsqueeze(im[:,i,:,:],axis=1))
+    imf2=g2(im)
     #imf2 = g2(im.detach())
     '''
     imdiff = imf1 - gamma * imf2
@@ -200,11 +199,11 @@ class LapStyleDraXDOG(BaseModel):
         self.style_layers = style_layers
         self.content_weight = content_weight
         self.style_weight = style_weight
-        self.gaussian_filter = paddle.nn.Conv2D(1, 1,9,
+        self.gaussian_filter = paddle.nn.Conv2D(3, 3,9,
                                 groups=1, bias_attr=False,
                                 padding=4, padding_mode='reflect')
-        self.gaussian_filter_2 = paddle.nn.Conv2D(1, 1,9,
-                                groups=1, bias_attr=False,
+        self.gaussian_filter_2 = paddle.nn.Conv2D(3, 3,9,
+                                groups=3, bias_attr=False,
                                 weight_attr=paddle.ParamAttr(initializer=paddle.nn.initializer.Normal(mean=0,std=.8*1.8),trainable=False),
                                 padding=4, padding_mode='reflect')
         self.gaussian_filter_2.set_state_dict({'weight':gaussian(10,.6)})

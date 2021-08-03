4d946a668462414c42b20fea7fde42c6a4cc3990
David Gill <davegill@Davids-MBP.home>
2021-08-03 03:16:03 -0400

    debug gaussian

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -38,8 +38,7 @@ def xdog(im, g, g2,morph_conv,gamma=1.5, phi=200, eps=-.5, k=1.6):
     #imf1 = paddle.concat(x=[g(paddle.unsqueeze(im[:,0,:,:].detach(),axis=1)),g(paddle.unsqueeze(im[:,1,:,:].detach(),axis=1)),g(paddle.unsqueeze(im[:,2,:,:].detach(),axis=1))],axis=1)
     imf2=paddle.zeros_like(im)
     imf2.stop_gradient=True
-    for i in range(im.shape[1]):
-        imf2[:,i,:,:]=g2(paddle.unsqueeze(im[:,i,:,:],axis=1))
+    imf2[:,i,:,:]=g2(im)
     #imf2 = g2(im.detach())
     '''
     imdiff = imf1 - gamma * imf2

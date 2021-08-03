aaba3e8d32f84e04d1141b2d424454660421c928
David Gill <davegill@Davids-MBP.home>
2021-08-03 02:34:51 -0400

    debug gaussian

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -184,14 +184,14 @@ class LapStyleDraXDOG(BaseModel):
         self.style_layers = style_layers
         self.content_weight = content_weight
         self.style_weight = style_weight
-        self.gaussian_filter = paddle.nn.Conv2D(1, 1,10,
+        self.gaussian_filter = paddle.nn.Conv2D(1, 1,9,
                                 groups=1, bias_attr=False,
                                 weight_attr=paddle.ParamAttr(initializer=paddle.nn.initializer.Normal(mean=1,std=.8),trainable=False),
-                                padding=5, padding_mode='reflect')
-        self.gaussian_filter_2 = paddle.nn.Conv2D(1, 1,10,
+                                padding=4, padding_mode='reflect')
+        self.gaussian_filter_2 = paddle.nn.Conv2D(1, 1,9,
                                 groups=1, bias_attr=False,
                                 weight_attr=paddle.ParamAttr(initializer=paddle.nn.initializer.Normal(mean=1,std=.8*20),trainable=False),
-                                padding=5, padding_mode='reflect')
+                                padding=4, padding_mode='reflect')
         self.morph_conv = paddle.nn.Conv2D(3,3,3,padding=1,groups=3,padding_mode='reflect',weight_attr=paddle.ParamAttr(initializer=paddle.nn.initializer.Bilinear(),trainable=False),bias_attr=False)
         self.set_requires_grad([self.morph_conv], False)
         self.set_requires_grad([self.gaussian_filter],False)

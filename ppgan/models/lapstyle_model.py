97bd5d2190b5f5487eac2f282dd4dd80e0a85caf
JackMcCoy <fdrdavegill@gmail.com>
2021-08-04 18:24:30 -0400

    debug

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -206,21 +206,16 @@ class LapStyleDraXDOG(BaseModel):
         self.style_layers = style_layers
         self.content_weight = content_weight
         self.style_weight = style_weight
-        self.gaussian_filter = paddle.static.nn.conv2d(1, 1,15,
+        self.gaussian_filter = paddle.nn.Conv2D(1, 1,15,
                                 groups=1, bias_attr=False,
                                 padding=7, padding_mode='reflect')
-        self.gaussian_filter_2 = paddle.static.nn.conv2d(1, 1,15,
+        self.gaussian_filter_2 = paddle.nn.Conv2D(1, 1,15,
                                 groups=1, bias_attr=False,
                                 padding=7, padding_mode='reflect')
-        self.gaussian_filter.set_state_dict({'weight':gaussian(15,1)})
-        self.gaussian_filter_2.set_state_dict({'weight':gaussian(15,1*5)})
-        self.morph_conv = paddle.static.nn.conv2d(1,1,7,padding=3,groups=1,padding_mode='reflect',bias_attr=False)
-        print(dir(self.morph_conv))
-        print(dir(self.morph_conv.weight))
-        print(self.morph_conv.state_dict())
-        print(dir(self.morph_conv.weight))
-        print(self.morph_conv.weight.__dict__)
-        self.morph_conv.weight = paddle.nn.ParamAttr(gaussian(7,1))
+        self.gaussian_filter=self.gaussian_filter.set_state_dict({'weight':gaussian(15,1)})
+        self.gaussian_filter_2=self.gaussian_filter_2.set_state_dict({'weight':gaussian(15,1*5)})
+        self.morph_conv = paddle.nn.Conv2D(1,1,7,padding=3,groups=1,padding_mode='reflect',bias_attr=False)
+        self.morph_conv=self.morph_conv.set_state_dict({'weight':gaussian(7,1)})
         print(self.morph_conv.weight)
         self.set_requires_grad([self.morph_conv], False)
         self.set_requires_grad([self.gaussian_filter],False)

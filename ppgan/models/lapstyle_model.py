5d3a1273a015e373e4c429b45f2061b35bfbe4bd
JackMcCoy <fdrdavegill@gmail.com>
2021-08-04 18:25:39 -0400

    debug

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -212,10 +212,10 @@ class LapStyleDraXDOG(BaseModel):
         self.gaussian_filter_2 = paddle.nn.Conv2D(1, 1,15,
                                 groups=1, bias_attr=False,
                                 padding=7, padding_mode='reflect')
-        self.gaussian_filter=self.gaussian_filter.set_state_dict({'weight':gaussian(15,1)})
-        self.gaussian_filter_2=self.gaussian_filter_2.set_state_dict({'weight':gaussian(15,1*5)})
+        self.gaussian_filter.set_state_dict({'weight':gaussian(15,1)})
+        self..gaussian_filter_2.set_state_dict({'weight':gaussian(15,1*5)})
         self.morph_conv = paddle.nn.Conv2D(1,1,7,padding=3,groups=1,padding_mode='reflect',bias_attr=False)
-        self.morph_conv=self.morph_conv.set_state_dict({'weight':gaussian(7,1)})
+        self.morph_conv.set_state_dict({'weight':gaussian(7,1)})
         print(self.morph_conv.weight)
         self.set_requires_grad([self.morph_conv], False)
         self.set_requires_grad([self.gaussian_filter],False)

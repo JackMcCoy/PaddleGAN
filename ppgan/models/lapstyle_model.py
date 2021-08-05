d2cd5f4b9d01f152b75a0cc09fcfcf6bf2575120
David Gill <davegill@Davids-MBP.home>
2021-08-05 12:39:19 -0400

    1/10th mdog weights

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -276,7 +276,7 @@ class LapStyleDraXDOG(BaseModel):
         self.losses['loss_c'] = self.loss_c
         """style loss"""
         self.loss_s = 0
-        for layer in self.style_layers[:-1]:
+        for layer in self.style_layers:
             self.loss_s += self.calc_style_loss(self.tF[layer], self.sF[layer])
         self.losses['loss_s'] = self.loss_s
         """IDENTITY LOSSES"""

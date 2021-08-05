5c107a00f441850af57922a57c9500c0f9d9d3d8
David Gill <davegill@Davids-MBP.home>
2021-08-05 05:26:39 -0400

    test slightly higher gaussian std, and 13 kernel size

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -217,13 +217,13 @@ class LapStyleDraXDOG(BaseModel):
                                 padding=6, padding_mode='reflect',
                                                 weight_attr=paddle.ParamAttr(
                                                     initializer=paddle.fluid.initializer.NumpyArrayInitializer(
-                                                        value=gaussian(15, 1).numpy()), trainable=False)
+                                                        value=gaussian(13, 1).numpy()), trainable=False)
                                                 )
         self.gaussian_filter_2 = paddle.nn.Conv2D(1, 1,13,
                                 groups=1, bias_attr=False,
                                 padding=6, padding_mode='reflect',
                                 weight_attr = paddle.ParamAttr(
-                                        initializer=paddle.fluid.initializer.NumpyArrayInitializer(value=gaussian(15, 1*1.8).numpy()), trainable=False)
+                                        initializer=paddle.fluid.initializer.NumpyArrayInitializer(value=gaussian(13, 1*1.8).numpy()), trainable=False)
                                     )
 
         self.morph_conv = paddle.nn.Conv2D(1,1,3,padding=1,groups=1,

11e24dc64cfba46cbfcfd2498602d613d61837b6
David Gill <davegill@Davids-MBP.home>
2021-08-05 09:47:59 -0400

    content minor weights=.23

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -266,7 +266,7 @@ class LapStyleDraXDOG(BaseModel):
         """content loss"""
         self.loss_c = 0
         for idx, layer in enumerate(self.content_layers[:-1]):
-            w = .28
+            w = .23
             if idx==2:
                 w=1
             self.loss_c += self.calc_content_loss(self.tF[layer],

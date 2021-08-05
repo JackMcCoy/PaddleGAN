cdac3b3b9c28f2ff4b65c0882598ed9601eadc25
David Gill <davegill@Davids-MBP.home>
2021-08-05 12:38:44 -0400

    1/10th mdog weights

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -267,7 +267,7 @@ class LapStyleDraXDOG(BaseModel):
         """content loss"""
         self.loss_c = 0
         for idx, layer in enumerate(self.content_layers[:-1]):
-            w = .23
+            w = .4
             if idx==2:
                 w=1
             self.loss_c += self.calc_content_loss(self.tF[layer],

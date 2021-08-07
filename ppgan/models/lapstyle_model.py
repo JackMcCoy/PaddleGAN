ce801136dfaaf3730eb72256b395c79d0113ce3b
David Gill <davegill@Davids-MBP.home>
2021-08-07 02:27:24 -0400

    gaus

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -55,7 +55,7 @@ def xdog(im, g, g2,morph_conv,gamma=.94, phi=50, eps=-.1, diff=False, position=F
     mean=paddle.expand_as(mean,morphed)
     passedlow= paddle.multiply((imdiff>= mean).astype('float32'),(morphed>= 78).astype('float32'))
     passed = morph_conv(passedlow)
-    passed= (passed>0).astype('float32')
+    passed= (imdiff>= mean).astype('float32')*(passed>0).astype('float32')
     return passed, imdiff
 
 def gaussian(kernel_size, sigma,channels=3):

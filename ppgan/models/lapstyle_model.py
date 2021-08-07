7b5c4099c99f5248801a3fe4a42d4cf7bb920d3b
David Gill <davegill@Davids-MBP.home>
2021-08-07 07:54:34 -0400

    lap filter

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -869,7 +869,7 @@ class LapStyleRevFirstThumb(BaseModel):
                                             initializer=paddle.fluid.initializer.Constant(
                                                             value=1), trainable=False)
                                         )
-            l = np.repeat(np.array([[-8,-8,-8],[-8,1,-8],[-8,-8,-8]]).reshape([1,1,3,3]),3,axis=0)
+            l = np.repeat(np.repeat(np.array([[-8,-8,-8],[-8,1,-8],[-8,-8,-8]]),3,axis=0),3,axis=0)
             self.lap_filter = paddle.nn.Conv2D(3,3,(3,3),stride=1,bias_attr=False,
                                     padding=1, padding_mode='reflect',
                                     weight_attr = paddle.ParamAttr(

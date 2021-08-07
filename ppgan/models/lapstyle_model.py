a2daf21ea5fb751120f64ce0b3f96649d48fab85
David Gill <davegill@Davids-MBP.home>
2021-08-06 22:26:49 -0400

    numpy repeat

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -883,7 +883,7 @@ class LapStyleRevFirstThumb(BaseModel):
                                             initializer=paddle.fluid.initializer.NumpyArrayInitializer(
                                                 value=np.repeat(
                                                     np.array([[[[-8,-8,-8],[-8,1,-8],[-8,-8,-8]]]])
-                                                    ,2,axis=1)
+                                                    ,3,axis=1)
 
                                         ), trainable=False)
                                                )

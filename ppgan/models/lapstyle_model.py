f685142fc59391ecbba44ae8a330d7080f2865d6
David Gill <davegill@Davids-MBP.home>
2021-08-07 01:26:02 -0400

    debug

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -52,6 +52,8 @@ def xdog(im, g, g2,morph_conv,gamma=.94, phi=50, eps=-.1, diff=False, position=F
     morphed.stop_gradient=True
     mean = imdiff.mean(axis=[0,1,2],keepdim=True)
     mean=paddle.expand_as(mean,morphed)
+    print(imdiff)
+    print(morphed)
     passedlow= paddle.greater_than(imdiff, mean).astype('float32')*paddle.greater_than(morphed, mean*81*3*5).astype('float32')
     passed = morph_conv(passedlow)
     passed= (passed>=1).astype('float32')

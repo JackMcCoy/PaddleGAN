6f6f1e555f10986de2cb08f0fae567e8d7d4e547
David Gill <davegill@Davids-MBP.home>
2021-08-09 00:27:42 -0400

    threshold=76

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -2355,7 +2355,7 @@ class LapStyleRevSecondMXDOG(BaseModel):
         self.loss_ps = 0
         self.p_loss_style_remd = 0
         if type(self.cX)==bool:
-            self.cX = xdog(self.content.detach(),self.gaussian_filter,self.gaussian_filter_2,self.morph_conv_2,morph_cutoff=79,morphs=1)
+            self.cX = xdog(self.content.detach(),self.gaussian_filter,self.gaussian_filter_2,self.morph_conv_2,morph_cutoff=76,morphs=1)
         for j in range(i+1):
             cX = paddle.slice(self.cX,axes=[2,3],starts=[self.positions[j][1].astype('int32'),self.positions[j][0].astype('int32')],ends=[self.positions[j][3].astype('int32'),self.positions[j][2].astype('int32')])
         cX = F.interpolate(cX,size=(256,256))

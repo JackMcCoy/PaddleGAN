d94be8e04808d40734042c65711456bebb46e202
David Gill <davegill@Davids-MBP.home>
2021-08-09 13:36:10 -0400

    reverse style calls because style_stack decreases in resolution

--- ppgan/datasets/lapstyle_dataset.py
+++ ppgan/datasets/lapstyle_dataset.py
@@ -373,15 +373,13 @@ class MultiPatchSet(Dataset):
 
         style_stack.append(self.img(np.array(style_img.resize((self.crop_size*2,self.crop_size*2)))))
 
-        pos=get_crop_bounds(self.crop_size*2,style_img.width,style_img.height)
+        pos=get_crop_bounds(self.crop_size*4,style_img.width,style_img.height)
         style_patch = np.array(style_img.crop(box=(pos[0],pos[1],pos[2],pos[3])).resize((self.crop_size*2,self.crop_size*2)))
         style_stack.append(self.img(self.transform_patch(style_patch)))
 
         style_patch = style_img
         pos=get_crop_bounds(self.crop_size*2,style_img.width,style_img.height)
         style_patch = style_patch.crop(box=(pos[0],pos[1],pos[2],pos[3]))
-        pos=get_crop_bounds(self.crop_size*2,style_patch.width,style_patch.height)
-        style_patch = np.array(self.transform_patch(style_patch.crop(box=(pos[0],pos[1],pos[2],pos[3]))))
         style_stack.append(self.img(style_patch))
         output = {}
         for idx,i in enumerate(content_stack):

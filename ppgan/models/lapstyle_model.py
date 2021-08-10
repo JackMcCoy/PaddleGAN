3f018b7e12d896caab3fe617b61b14c818253c33
JackMcCoy <fdrdavegill@gmail.com>
2021-08-09 23:44:06 -0400

    scale up gan 1.8

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -2428,7 +2428,7 @@ class LapStyleRevSecondMXDOG(BaseModel):
         self.loss = self.loss_Gp_GAN*((i+1.8) * self.gan_thumb_weight) +self.loss_ps/4 * self.style_weight +\
                     self.loss_content_p * self.content_weight +\
                     self.loss_patch +\
-                    self.p_loss_style_remd/4 * 26 + self.p_loss_content_relt * 26 + mxdogloss
+                    self.p_loss_style_remd/4 * 28 + self.p_loss_content_relt * 28 + mxdogloss
         self.loss.backward()
 
         return self.loss

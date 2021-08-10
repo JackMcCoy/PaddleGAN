a0917e9e5b502b334b28a23571f6bb431e92233c
David Gill <davegill@Davids-MBP.home>
2021-08-10 06:39:52 -0400

    decrease mdog for second layer by half

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -2741,7 +2741,7 @@ class LapStyleRevSecondMiddle(BaseModel):
         self.loss = self.loss_Gp_GAN *self.gan_thumb_weight +self.loss_ps/4 * self.style_weight +\
                     self.loss_content_p * self.content_weight +\
                     self.loss_patch +\
-                    self.p_loss_style_remd/4 * 22 + self.p_loss_content_relt * 22 + mxdogloss
+                    self.p_loss_style_remd/4 * 22 + self.p_loss_content_relt * 22 + mxdogloss/(i+1)
 
         return self.loss
 

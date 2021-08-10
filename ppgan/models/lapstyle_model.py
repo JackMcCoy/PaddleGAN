bc590e39eb4ce93bb770e7e46b4ea852896d3aaf
JackMcCoy <fdrdavegill@gmail.com>
2021-08-10 00:48:07 -0400

    exponential style weight additions

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -2425,7 +2425,7 @@ class LapStyleRevSecondMXDOG(BaseModel):
         self.losses['loss_gan_Gp_'+str(i+1)] = self.loss_Gp_GAN*self.gan_thumb_weight
 
 
-        self.loss = self.loss_Gp_GAN*((i+1.8) * self.gan_thumb_weight) +self.loss_ps/4 * self.style_weight*(2**(1.5+i)-3) +\
+        self.loss = self.loss_Gp_GAN*((i+1.8) * self.gan_thumb_weight) +self.loss_ps/4 * self.style_weight*(2**(.5+i)) +\
                     self.loss_content_p * self.content_weight +\
                     self.loss_patch +\
                     self.p_loss_style_remd/4 * 16 + self.p_loss_content_relt * 16 + mxdogloss

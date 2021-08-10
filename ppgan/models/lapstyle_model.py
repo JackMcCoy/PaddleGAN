40a6651de92d673cb77a4aecb8fc15a5a51cbffe
David Gill <davegill@Davids-MBP.home>
2021-08-10 17:49:48 -0400

    cX out

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -2435,7 +2435,7 @@ class LapStyleRevSecondMXDOG(BaseModel):
         self.loss = self.loss_Gp_GAN *self.gan_thumb_weight +self.loss_ps/4 * self.style_weight +\
                     self.loss_content_p * self.content_weight +\
                     self.loss_patch +\
-                    self.p_loss_style_remd/4 * 10 + self.p_loss_content_relt * 16
+                    self.p_loss_style_remd/4 * 28 + self.p_loss_content_relt * 28
 
         return self.loss
 

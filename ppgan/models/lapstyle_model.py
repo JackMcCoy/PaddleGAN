2598a1cf4d7b7a8402525e322d568020e8fad4db
JackMcCoy <fdrdavegill@gmail.com>
2021-08-09 20:10:46 -0400

    use 7size kernel for style

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -2428,7 +2428,7 @@ class LapStyleRevSecondMXDOG(BaseModel):
         self.loss = self.loss_Gp_GAN *(self.gan_thumb_weight+(i*1.6)) +self.loss_ps/4 * self.style_weight +\
                     self.loss_content_p * self.content_weight +\
                     self.loss_patch +\
-                    self.p_loss_style_remd/4 * 18 + self.p_loss_content_relt * 18 + mxdogloss
+                    self.p_loss_style_remd/4 * (18*(i+1.6)) + self.p_loss_content_relt * (18*(i+1.6)) + mxdogloss
         self.loss.backward()
 
         return self.loss

84ecd076a4aefb9041be79ad0912d75d537f388b
David Gill <davegill@Davids-MBP.home>
2021-08-11 05:51:26 -0400

    patch loss scale change

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -2435,7 +2435,7 @@ class LapStyleRevSecondMXDOG(BaseModel):
         self.loss = self.loss_Gp_GAN*(i*10*i) *self.gan_thumb_weight +self.loss_ps/4 * self.style_weight +\
                     self.loss_content_p * self.content_weight +\
                     self.loss_patch*(i*10) +\
-                    self.p_loss_style_remd/4 * 28/(3-i+1) + self.p_loss_content_relt * 28 + (mxdogloss/(max(2*50**i,1)))
+                    self.p_loss_style_remd/4 * 28/(3-i+1) + self.p_loss_content_relt * 28 + (mxdogloss/(max(2*100**i,1)))
 
         return self.loss
 

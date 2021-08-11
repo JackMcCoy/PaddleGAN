288d4143795958fb275cb8e606a5d777a777ff52
David Gill <davegill@Davids-MBP.home>
2021-08-11 03:49:00 -0400

    scale down mdog loss with depth

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -2435,7 +2435,7 @@ class LapStyleRevSecondMXDOG(BaseModel):
         self.loss = self.loss_Gp_GAN*(i*10*i) *self.gan_thumb_weight +self.loss_ps/4 * self.style_weight +\
                     self.loss_content_p * self.content_weight +\
                     self.loss_patch*(i*10) +\
-                    self.p_loss_style_remd/4 * 14 + self.p_loss_content_relt * 28 + mxdogloss
+                    self.p_loss_style_remd/4 * 14 + self.p_loss_content_relt * 28 + mxdogloss/(i*10)
 
         return self.loss
 

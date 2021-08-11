50d8a4439c0292006be53fd8ac53368749c43d8d
JackMcCoy <fdrdavegill@gmail.com>
2021-08-10 23:19:59 -0400

    decoder kmeans

--- ppgan/models/generators/generater_lapstyle.py
+++ ppgan/models/generators/generater_lapstyle.py
@@ -230,8 +230,8 @@ class DecoderKMeans(nn.Layer):
         cs = []
         print(ci.shape)
         for i in range(ci.shape[0]):
-            cp = paddle.unsqueeze(ci[i,:,:,:],axis=0)
-            sp = paddle.unsqueeze(si[i, :, :, :],axis=0)
+            cp = ci[i,:,:,:],axis=0
+            sp = si[i, :, :, :],axis=0
             print(cp.shape)
             content_label, content_center_norm = calc_k(cp)
             style_label, style_center_norm = calc_k(sp)

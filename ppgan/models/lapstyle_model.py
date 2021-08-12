466bb73150b2490a27afba946b71a2f991b2e8fe
JackMcCoy <fdrdavegill@gmail.com>
2021-08-12 12:49:55 -0400

    patch loss = 2.75

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -2433,7 +2433,7 @@ class LapStyleRevSecondMXDOG(BaseModel):
 
         self.loss = self.loss_Gp_GAN +self.loss_ps/4 * self.style_weight +\
                     self.loss_content_p * self.content_weight +\
-                    self.loss_patch * (2+i*.75) +\
+                    self.loss_patch * 2.75 +\
                     self.p_loss_style_remd/4 * 10.5 + self.p_loss_content_relt * 16 + mxdogloss
 
         return self.loss

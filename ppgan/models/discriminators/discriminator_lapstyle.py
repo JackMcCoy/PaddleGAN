1e9fd8a5f5dba6894077574d16642555d73ff7b4
JackMcCoy <fdrdavegill@gmail.com>
2021-08-15 19:36:24 -0400

    debug

--- ppgan/models/discriminators/discriminator_lapstyle.py
+++ ppgan/models/discriminators/discriminator_lapstyle.py
@@ -181,7 +181,7 @@ class LapStyleSpectralDiscriminator(nn.Layer):
                 'conv%d' % (i + 1),
                 ResBlock(num_channel))
         self.tail = nn.ReLU()
-        self.fc = nn.Sequential(nn.SpectralNorm((1024,1)),nn.Linear(num_channels , 1),nn.Sigmoid())
+        self.fc = nn.Sequential(nn.SpectralNorm((1,64)),nn.Linear(num_channels , 1),nn.Sigmoid())
 
     def forward(self, x):
         x = self.head(x)

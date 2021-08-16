af5be97dcc319230c372d7c840ab5ac1512836e5
JackMcCoy <fdrdavegill@gmail.com>
2021-08-15 20:19:33 -0400

    remove debug

--- ppgan/models/discriminators/discriminator_lapstyle.py
+++ ppgan/models/discriminators/discriminator_lapstyle.py
@@ -186,10 +186,7 @@ class LapStyleSpectralDiscriminator(nn.Layer):
     def forward(self, x):
         x = self.head(x)
         x = self.body(x)
-        print(x.shape)
         x = paddle.reshape(x,(-1,1024))
-        print(x.shape)
         x = self.fc(x)
-        print(x.shape)
         x = paddle.reshape(x,(1,1,256,256))
         return x

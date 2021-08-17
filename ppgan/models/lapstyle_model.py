553c62547e8f6f533c3729056026550bd4ac8867
David Gill <davegill@Davids-MBP.home>
2021-08-17 01:50:34 -0400

    ada alpha=.5

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -2365,7 +2365,7 @@ class LapStyleRevSecondMXDOG(BaseModel):
         self.losses['loss_MD_'+str(i+1)] = mxdog_content*.01
         self.losses['loss_CnsC_'+str(i+1)] = mxdog_content_contraint*50
         self.losses['loss_CnsS_'+str(i+1)] = mxdog_style*51.5
-        mxdogloss=mxdog_content * .3 + mxdog_content_contraint *25 + mxdog_style * 250
+        mxdogloss=mxdog_content * .25 + mxdog_content_contraint *25 + mxdog_style * 250
 
         """gan loss"""
         self.loss_Gp_GAN=0
@@ -2377,13 +2377,13 @@ class LapStyleRevSecondMXDOG(BaseModel):
 
         if i==0:
             a=10
-            b=18
+            b=16
             c=1
             d=1.1
         elif i>0 and i<3:
             a=10
-            b=18
-            c=1.5
+            b=16
+            c=1
             d=1.1
         else:
             a=28

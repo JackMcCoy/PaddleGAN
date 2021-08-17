6f4a0087c4ce0d1391adcdb4a53597ef35523734
David Gill <davegill@Davids-MBP.home>
2021-08-17 15:17:52 -0400

    ada_alpha=.05

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -2433,8 +2433,8 @@ class LapStyleRevSecondMXDOG(BaseModel):
     def backward_Dec(self):
         ci = F.interpolate(self.content_stack[0],scale_factor=.5)
         si = F.interpolate(self.style_stack[0],scale_factor=.5)
-        cF = self.nets['net_enc'](ci)
-        sF = self.nets['net_enc'](si)
+        cF = self.nets['net_enc'](ci.detach())
+        sF = self.nets['net_enc'](si.detach())
         tF = self.nets['net_enc'](self.stylized[0])
         """content loss"""
         loss_c = 0

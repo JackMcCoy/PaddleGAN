492ef2a64ec08ca4d96127c20b91a26bddab9ff6
David Gill <davegill@Davids-MBP.home>
2021-08-19 13:37:25 -0400

    increase second mdog content loss for firstpatch

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -694,9 +694,9 @@ class LapStyleRevFirstMXDOG(BaseModel):
         mxdog_content_img = self.gram_errors(self.cdogF['r31'],self.sXF['r31'])
 
         self.losses['loss_MD_p'] = mxdog_content*.3
-        self.losses['loss_CnsC_p'] = mxdog_content_contraint*50
+        self.losses['loss_CnsC_p'] = mxdog_content_contraint*100
         self.losses['loss_CnsS_p'] = mxdog_content_img*1000
-        mxdogloss=mxdog_content * .3 + mxdog_content_contraint *50 + mxdog_content_img * 1000
+        mxdogloss=mxdog_content * .3 + mxdog_content_contraint *100 + mxdog_content_img * 1000
 
         self.loss = self.loss_G_GAN + self.loss_c * self.content_weight + self.style_weight * (self.loss_s +\
                     self.loss_style_remd * 3) + self.loss_content_relt * 20 + mxdogloss

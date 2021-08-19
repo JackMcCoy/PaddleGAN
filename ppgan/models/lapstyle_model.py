1f2b4b40465183ba324be606d550bfcaf60c858a
David Gill <davegill@Davids-MBP.home>
2021-08-19 03:27:39 -0400

    gram_error draft and rev first

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -319,11 +319,11 @@ class LapStyleDraXDOG(BaseModel):
 
         self.losses['loss_MD'] = mxdog_content*.3
         self.losses['loss_CnsC'] = mxdog_content_contraint*100
-        self.losses['loss_CnsS'] = mxdog_content_img*500
+        self.losses['loss_CnsS'] = mxdog_content_img*1000
 
         self.loss = self.loss_c * self.content_weight + self.style_weight * (self.loss_s +1.75*self.loss_style_remd)+\
                     self.l_identity1 * 50 + self.l_identity2 * 1 + \
-                    mxdog_content * .3 + mxdog_content_contraint *100 + mxdog_content_img * 500+\
+                    mxdog_content * .3 + mxdog_content_contraint *100 + mxdog_content_img * 1000+\
                     self.loss_content_relt * 20
         self.loss.backward()
 

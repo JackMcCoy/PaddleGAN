c2fc4ef6d66fc1014a471ef3b9d7322c818d7035
JackMcCoy <fdrdavegill@gmail.com>
2021-08-20 18:46:54 -0400

    debug spectrallinear

--- ppgan/models/discriminators/discriminator_lapstyle.py
+++ ppgan/models/discriminators/discriminator_lapstyle.py
@@ -126,7 +126,7 @@ def max_singular_value(W, u=None, Ip=1):
     _u = u
     print(W.shape)
     for _ in range(Ip):
-        _v = _l2normalize(paddle.matmul(_u, W), eps=1e-12)
+        _v = _l2normalize(paddle.matmul(_u, paddle.transpose(W.[1,0])), eps=1e-12)
         _u = _l2normalize(paddle.matmul(_v, paddle.transpose(W, [1, 0])), eps=1e-12)
     sigma = paddle.sum(nn.functional.linear(_u, paddle.transpose(W, [1, 0])) * _v)
     return sigma, _u
@@ -157,8 +157,6 @@ class SNLinear(Linear):
     @property
     def W_(self):
         w_mat = paddle.reshape(self.weight,(self.weight.shape[0], -1))
-        print(self.u)
-        print(self.weight)
         sigma, _u = max_singular_value(w_mat, self.u)
         self.u.copy_(_u)
         return self.weight / sigma

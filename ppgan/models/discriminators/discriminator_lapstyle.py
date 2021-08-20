069bb98ce06b738804b3e9b3a4f45a55affc8cbc
David Gill <davegill@Davids-MBP.home>
2021-08-20 17:30:54 -0400

    add spectral disc

--- ppgan/models/discriminators/discriminator_lapstyle.py
+++ ppgan/models/discriminators/discriminator_lapstyle.py
@@ -182,7 +182,6 @@ class LapStyleSpectralDiscriminator(nn.Layer):
         x = self.head(x)
         x = self.body(x)
         x = self.fc(x)
-        print(x.shape)
-        x = nn.functional.avg_pool2d(x, 256, stride=1)
+        x = nn.functional.avg_pool2d(x, 3, stride=1)
         x = paddle.reshape(x,(1,1,256,256))
         return x

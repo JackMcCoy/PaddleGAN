6546bbf81b3cc93cc56e5224a14c39d372e472d7
JackMcCoy <fdrdavegill@gmail.com>
2021-08-19 23:41:24 -0400

    rev 2

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -2232,7 +2232,7 @@ class LapStyleRevSecondMXDOG(BaseModel):
             self.positions = input['position_stack']
             self.size_stack = input['size_stack']
             self.laplacians.append(laplacian_conv(self.content_stack[0],self.lap_filter).detach())
-            #self.laplacians.append(laplacian_conv(self.content_stack[1],self.lap_filter).detach())
+            self.laplacians.append(laplacian_conv(self.content_stack[1],self.lap_filter).detach())
             #self.laplacians.append(laplacian_conv(self.content_stack[2],self.lap_filter).detach())
             #self.laplacians.append(laplacian_conv(self.content_stack[3],self.lap_filter).detach())
             self.cX = False

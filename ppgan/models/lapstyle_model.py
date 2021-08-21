ec2d27e6601e3e3dc7ecfb35ee85a8910ef3824e
David Gill <davegill@Davids-MBP.home>
2021-08-20 23:34:50 -0400

    lower lr

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -2409,17 +2409,17 @@ class LapStyleRevSecondMXDOG(BaseModel):
         self.losses['loss_gan_Gp_'+str(i+1)] = self.loss_Gp_GAN*self.gan_thumb_weight
 
         if i==0:
-            a=12
+            a=13
             b=16
             c=1
             d=0
         elif i>0 and i<3:
-            a=22
+            a=23
             b=26
             c=5
             d=3
         else:
-            a=24
+            a=25
             b=28
             c=10
             d=6

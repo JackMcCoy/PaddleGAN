01c8e8e48635a313d44178a58ec49bf0ce213c0c
JackMcCoy <fdrdavegill@gmail.com>
2021-08-21 12:30:19 -0400

    increase mdog content

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -2409,7 +2409,7 @@ class LapStyleRevSecondMXDOG(BaseModel):
         self.losses['loss_gan_Gp_'+str(i+1)] = self.loss_Gp_GAN*self.gan_thumb_weight
 
         if i==0:
-            a=13.5
+            a=12
             b=16
             c=1
             d=0

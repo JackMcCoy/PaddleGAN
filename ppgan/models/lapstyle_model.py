e2dd9599bda97bcb49d79f49cf02bf38cf89893d
David Gill <davegill@Davids-MBP.home>
2021-08-22 06:41:31 -0400

    three_eps test

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -36,7 +36,7 @@ def xdog(im, g, g2,morph_conv,gamma=.94, phi=50, eps=-.5, morph_cutoff=8.88,morp
     # Link : http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.365.151&rep=rep1&type=pdf
     #imf1 = paddle.concat(x=[g(paddle.unsqueeze(im[:,0,:,:].detach(),axis=1)),g(paddle.unsqueeze(im[:,1,:,:].detach(),axis=1)),g(paddle.unsqueeze(im[:,2,:,:].detach(),axis=1))],axis=1)
     if not type(three_eps)==bool:
-        eps = paddle.to_tensor([[three_eps[0]],[three_eps[1]],[three_eps[2]]])
+        eps = paddle.to_tensor([[[three_eps[0]],[three_eps[1]],[three_eps[2]]]])
     imf2=paddle.zeros_like(im)
     imf1=paddle.zeros_like(im)
     imf1.stop_gradient=True

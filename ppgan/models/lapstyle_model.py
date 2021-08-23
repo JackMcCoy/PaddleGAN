64b7d9e65c81f47dd54dd9f966b1baae3b070629
David Gill <davegill@Davids-MBP.home>
2021-08-22 23:04:34 -0400

    train spectral switch

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -2433,8 +2433,10 @@ class LapStyleRevSecondMXDOG(BaseModel):
         self.loss_Gp_GAN=0
         pred_fake_p = self.nets[self.discriminators[-1]](self.stylized[i+1])
         self.loss_Gp_GAN += self.gan_criterion(pred_fake_p, True)
-        pred_fake_p = self.nets['spectral_D'](self.stylized[i+1])
-        self.loss_Gs_GAN = self.gan_criterion(pred_fake_p, True)
+        self.loss_Gs_GAN = 0
+        if self.train_spectral==1:
+            pred_fake_p = self.nets['spectral_D'](self.stylized[i+1])
+            self.loss_Gs_GAN += self.gan_criterion(pred_fake_p, True)
 
         if i==0:
             a=11

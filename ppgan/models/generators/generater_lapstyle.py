a9cae13ff36277b79748f7b4643179aaca705eb9
David Gill <davegill@Davids-MBP.home>
2021-08-25 16:39:33 -0400

    32 bit

--- ppgan/models/generators/generater_lapstyle.py
+++ ppgan/models/generators/generater_lapstyle.py
@@ -20,7 +20,7 @@ from skimage import color
 import numpy as np
 from .builder import GENERATORS
 
-if 1:
+if 0:
     import pycuda.autoinit
     import pycuda.gpuarray as gpuarray
     import skcuda.linalg as linalg

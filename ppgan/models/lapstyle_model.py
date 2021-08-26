eddee8ecceec1b9a5eedae5bc809fd8be1b7c1ab
JackMcCoy <fdrdavegill@gmail.com>
2021-08-26 00:48:36 -0400

    wider gaussian kernels

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -230,7 +230,7 @@ class LapStyleDraXDOG(BaseModel):
         g2 = np.repeat(gaussian(21, 3).numpy(), 3, axis=0)
         self.gaussian_filter = paddle.nn.Conv2D(3, 3, 9,
                                                 groups=3, bias_attr=False,
-                                                padding=3, padding_mode='reflect',
+                                                padding=4, padding_mode='reflect',
                                                 weight_attr=paddle.ParamAttr(
                                                     initializer=paddle.fluid.initializer.NumpyArrayInitializer(
                                                         value=g), trainable=False)

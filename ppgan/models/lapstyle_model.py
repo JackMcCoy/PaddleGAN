90cfe1cce0f4ae6d659d0d79c8bf487e4083b80a
JackMcCoy <fdrdavegill@gmail.com>
2021-08-28 02:53:33 -0400

    patch = gan loss

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -2448,7 +2448,7 @@ class LapStyleRevSecondMXDOG(BaseModel):
             a=16
             b=16
             c=(1.5*(i+1))
-            d=5+i
+            d=(1.5*(i+1))
         else:
             a=26
             b=26

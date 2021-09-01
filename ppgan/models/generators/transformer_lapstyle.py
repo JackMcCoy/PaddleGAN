350bcbb8bdeac3d64b9c1bde389dafa8aabfc4aa
David Gill <davegill@Davids-MBP.home>
2021-08-31 23:52:17 -0400

    try ViT

--- ppgan/models/generators/transformer_lapstyle.py
+++ ppgan/models/generators/transformer_lapstyle.py
@@ -97,8 +97,8 @@ class ViT(nn.Layer):
         assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
 
         self.to_patch_embedding = nn.Sequential(
-            [Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
-            nn.Linear(patch_dim, dim)]
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+            nn.Linear(patch_dim, dim),
         )
 
         self.pos_embedding = paddle.create_parameter((1, num_patches + 1, dim), dtype='float32')

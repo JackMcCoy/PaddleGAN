e223534152ef0d8cdd623b4cd771d88a106e6358
David Gill <davegill@Davids-MBP.home>
2021-09-01 02:38:33 -0400

    remove torch ref

--- ppgan/models/generators/transformer_lapstyle.py
+++ ppgan/models/generators/transformer_lapstyle.py
@@ -143,4 +143,6 @@ class ViT(nn.Layer):
         x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
 
         x = self.to_latent(x)
-        return self.mlp_head(x)
+        x = self.mlp_head(x)
+        print(x.shape)
+        return x

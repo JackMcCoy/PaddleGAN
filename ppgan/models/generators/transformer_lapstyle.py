e0919876d9cce7ce450547b54ad75320cec8282b
David Gill <davegill@Davids-MBP.home>
2021-09-01 03:14:25 -0400

    remove torch ref

--- ppgan/models/generators/transformer_lapstyle.py
+++ ppgan/models/generators/transformer_lapstyle.py
@@ -110,9 +110,7 @@ class ViT(nn.Layer):
         assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
 
         self.rearrange=Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width)
-        self.unroll=Rearrange('b (h w) (p1 p2 c) -> b c (h p1) (w p2)', p1 = patch_height, p2 = patch_width)
-
-
+        
         self.to_patch_embedding = nn.Linear(patch_dim, dim)
 
         self.pos_embedding = paddle.create_parameter(shape=(1, num_patches + 1, dim), dtype='float32')

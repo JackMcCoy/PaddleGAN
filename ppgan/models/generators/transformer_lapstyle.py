74a69e508299c7b5ddd8d5aa3b6c892231809a07
David Gill <davegill@Davids-MBP.home>
2021-09-02 18:42:30 -0400

    change einops rearrange code

--- ppgan/models/generators/transformer_lapstyle.py
+++ ppgan/models/generators/transformer_lapstyle.py
@@ -154,7 +154,7 @@ class ViT(nn.Layer):
         assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
 
         self.rearrange=Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width)
-        self.decompose_axis=Rearrange('b (h w) c -> b c h w', w=(image_width // patch_width))
+        self.decompose_axis=Rearrange('(h w) (p1 p2 c) -> c (h p1) (w p2)', w=(image_width // patch_width),p1=patch_height,p2=patch_width)
         self.to_patch_embedding = nn.Linear(patch_dim, dim)
 
         self.pos_embedding = paddle.create_parameter(shape=(1, num_patches + 1, dim), dtype='float32')

76ab02d4a2a4c18964562c4a676430c4f7f5c6e5
David Gill <davegill@Davids-MBP.home>
2021-09-03 23:04:44 -0400

    16 heads

--- ppgan/models/generators/transformer_lapstyle.py
+++ ppgan/models/generators/transformer_lapstyle.py
@@ -223,7 +223,7 @@ class ViTDraft(nn.Layer):
 
         self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
 
-        decoder_layer = nn.TransformerDecoderLayer(1024, 2, 1024,normalize_before=True)
+        decoder_layer = nn.TransformerDecoderLayer(1024, 16, 1024,normalize_before=True)
         self.decoder_transformer = nn.TransformerDecoder(decoder_layer, 6)
 
         dec_input = paddle.rand((5, 65, 1024))

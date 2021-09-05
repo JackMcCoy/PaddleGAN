e83c023ee8be4e35016cc49f7da1849febaee295
JackMcCoy <fdrdavegill@gmail.com>
2021-09-05 19:03:09 -0400

    no decoder transformer

--- ppgan/models/generators/transformer_lapstyle.py
+++ ppgan/models/generators/transformer_lapstyle.py
@@ -42,7 +42,7 @@ class Attention(nn.Layer):
         super().__init__()
         inner_dim = dim_head *  heads
         project_out = not (heads == 1 and dim_head == dim)
-
+        print(project_out)
         self.heads = heads
         self.scale = dim_head ** -0.5
 
@@ -50,7 +50,7 @@ class Attention(nn.Layer):
         self.to_qkv = nn.Linear(dim, inner_dim * 3, bias_attr = False)
 
         self.to_out = nn.Linear(inner_dim, dim)
-        self.d_o=nn.Dropout(dropout)
+        self.d_o=nn.Dropout(p=dropout)
 
     def forward(self, x):
         qkv = paddle.chunk(self.to_qkv(x),3, axis = -1)

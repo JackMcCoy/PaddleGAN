6f83b1683a1fc7b8ee6f4bcaf8811ff525dab00d
JackMcCoy <fdrdavegill@gmail.com>
2021-09-04 20:49:19 -0400

    crossvit

--- ppgan/models/generators/xvit.py
+++ ppgan/models/generators/xvit.py
@@ -228,17 +228,16 @@ class ImageEmbedder(nn.Layer):
         num_patches = (image_size // patch_size) ** 2
         patch_dim = 6 * patch_size ** 2
 
-        self.to_patch_embedding = nn.Sequential(
-            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_size, p2 = patch_size),
-            nn.Linear(patch_dim, dim),
-        )
+        self.rearrange=Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_size, p2 = patch_size)
+        self.to_patch_embedding =nn.Linear(patch_dim, dim)
 
         self.pos_embedding = nn.Parameter(paddle.randn(1, num_patches + 1, dim))
         self.cls_token = nn.Parameter(paddle.randn(1, 1, dim))
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, img):
-        x = self.to_patch_embedding(img)
+        x = self.rearrange(img)
+        x = self.to_patch_embedding(x)
         b, n, _ = x.shape
 
         cls_tokens = repeat(self.cls_token, '() n d -> b n d', b = b)

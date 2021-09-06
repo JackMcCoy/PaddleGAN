f079e5e5f9b8809ed953fddbbb1b94cfb2c1a9b3
JackMcCoy <fdrdavegill@gmail.com>
2021-09-06 18:18:44 -0400

    lower mxdog loss

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -296,14 +296,14 @@ class LapStyleDraXDOG(BaseModel):
             self.loss_s += self.calc_style_loss(self.tF[layer], self.sF[layer])
         self.losses['loss_s'] = self.loss_s
         """IDENTITY LOSSES"""
-        dual_si = paddle.concat(x=[self.si, self.si], axis=1)
-        self.Icc = self.nets['net_vit'](dual_si)
-        self.l_identity1 = self.calc_content_loss(self.Icc, self.si)
+        dual_ci = paddle.concat(x=[self.ci, self.ci], axis=1)
+        self.Icc = self.nets['net_vit'](dual_ci)
+        self.l_identity1 = self.calc_content_loss(self.Icc, self.ci)
         self.Fcc = self.nets['net_enc'](self.Icc)
         self.l_identity2 = 0
         for layer in self.content_layers:
             self.l_identity2 += self.calc_content_loss(self.Fcc[layer],
-                                                       self.sF[layer])
+                                                       self.cF[layer])
         self.losses['l_identity1'] = self.l_identity1
         self.losses['l_identity2'] = self.l_identity2
         """relative loss"""
@@ -323,10 +323,12 @@ class LapStyleDraXDOG(BaseModel):
         self.losses['loss_MD'] = mxdog_content*.3
         self.losses['loss_CnsC'] = mxdog_content_contraint*100
         self.losses['loss_CnsS'] = mxdog_content_img*3000
+        mxdog_losses = mxdog_content * .3 + mxdog_content_contraint *100 + mxdog_content_img * 1000
+        mxdog_weight = .5
 
         self.loss = self.loss_c * self.content_weight + self.style_weight * (self.loss_s +3*self.loss_style_remd)+\
-                    self.l_identity1 * 100 + self.l_identity2 * 1 + \
-                    mxdog_content * .3 + mxdog_content_contraint *100 + mxdog_content_img * 1000+\
+                    self.l_identity1 * 50 + self.l_identity2 * 1 + \
+                    mxdog_losses*mxdog_weight+\
                     self.loss_content_relt * 16
         self.loss.backward()
 

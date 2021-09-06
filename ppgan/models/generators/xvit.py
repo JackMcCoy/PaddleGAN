d0beaca02f44eb217198b67f4ab3d4252927de19
JackMcCoy <fdrdavegill@gmail.com>
2021-09-05 20:32:40 -0400

    lr=1e-5

--- ppgan/models/generators/xvit.py
+++ ppgan/models/generators/xvit.py
@@ -307,7 +307,7 @@ class CrossViT(nn.Layer):
         self.sm_decompose_axis = Rearrange('b (h w) (e d c) -> b c (h e) (w d)',h=8,d=16,e=16)
         self.partial_unfold = Rearrange('b (h w p1) c -> b (h w) (p1 c)', w=2,h=2,
                                         p1=16)
-        self.rearrange = Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=4, p2=4)
+        self.rearrange = Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=8, p2=8)
         self.sm_project = nn.Sequential(nn.LayerNorm(sm_dim),nn.Linear(sm_dim,768))
         self.lg_project = nn.Sequential(nn.LayerNorm(lg_dim),nn.Linear(lg_dim,768))
         #self.sm_decoder_transformer = nn.TransformerDecoder(sm_decoder_layer, 6)

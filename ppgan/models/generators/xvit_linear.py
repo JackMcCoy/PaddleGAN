7176f39b447e205cc8c9945bb0c0fa8c9b4bcdff
David Gill <davegill@Davids-MBP.home>
2021-09-08 03:57:11 -0400

    linearxvit

--- ppgan/models/generators/xvit_linear.py
+++ ppgan/models/generators/xvit_linear.py
@@ -78,7 +78,7 @@ def look_around(x, backward = 1, forward = 0, pad_value = -1, dim = 2):
     t = x.shape[1]
     dims = (len(x.shape) - dim) * (0, 0)
 
-    padded_x = F.pad(x, [dims], value= pad_value)
+    padded_x = F.pad(x, [*dims,backward], value= pad_value)
     tensors = [padded_x[:, ind:(ind + t), ...] for ind in range(forward + backward + 1)]
     return paddle.concat(tensors, axis=dim)
 

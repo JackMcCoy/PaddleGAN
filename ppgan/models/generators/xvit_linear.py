14212d8171554fcb673e290ecdcb9c5dd46e792e
David Gill <davegill@Davids-MBP.home>
2021-09-08 05:51:01 -0400

    linearxvit

--- ppgan/models/generators/xvit_linear.py
+++ ppgan/models/generators/xvit_linear.py
@@ -82,7 +82,7 @@ def look_around(x, cls=None,backward = 1, forward = 0, pad_value = -1, dim = 2):
     tensors = [padded_x[:, ind:(ind + t), :] for ind in range(forward + backward + 1)]
     tensors=paddle.concat(tensors, axis=dim)
     if not cls is None:
-        tensors = paddle.concat([cls,tensors],axis=3)
+        tensors = paddle.concat([cls,tensors],axis=2)
     return tensors
 
 def split_at_index(dim, index, t):
@@ -347,7 +347,7 @@ class LocalAttention(nn.Layer):
 
         bucket_fn = lambda t: t.reshape((b, windows, window_size, -1))
         bq, bk, bv = map(bucket_fn, (q, k, v))
-        cls_fn = lambda t: t.reshape((b, t, -1, 1))
+        cls_fn = lambda t: t.reshape((b, 1, 1, -1))
         v_cls,k_cls = map(cls_fn, (v_cls,k_cls))
 
         look_around_kwargs = {'backward': look_backward, 'forward': look_forward}

cdd97d69ea5e02f003a128ccf4648d167ddd71df
David Gill <davegill@Davids-MBP.home>
2021-09-08 07:29:43 -0400

    print optimizer state

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -351,11 +351,11 @@ class LapStyleDraXDOG(BaseModel):
                         if type(v2)==dict:
                             for k3,v3 in v2.items():
                                 print(' - -'+k3)
-                                if hasattr(v3,'shape'):
+                                if hasattr(v3,'shape') and hasattr(v3,'weight'):
                                     print(' - - shape: '+str(v3.weight.shape))
-                        if hasattr(v2,'shape'):
+                        if hasattr(v2,'shape') and hasattr(v2,'weight'):
                             print(' - shape: '+str(v2.weight.shape))
-                if hasattr(v,'shape'):
+                if hasattr(v,'shape') and hasattr(v,'weight'):
                     print(' shape: '+str(v.weight.shape))
 
 

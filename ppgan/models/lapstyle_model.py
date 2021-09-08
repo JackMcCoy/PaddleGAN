84b8d33ddb097e38b85f6413e1b5554f9ea8721d
David Gill <davegill@Davids-MBP.home>
2021-09-08 08:24:27 -0400

    amp

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -326,7 +326,7 @@ class LapStyleDraXDOG(BaseModel):
         self.losses['loss_CnsC'] = mxdog_content_contraint*100
         self.losses['loss_CnsS'] = mxdog_content_img*3000
         mxdog_losses = mxdog_content * .3 + mxdog_content_contraint *100 + mxdog_content_img * 1000
-        mxdog_weight = .5
+        mxdog_weight = .1
 
         self.loss = self.loss_c * self.content_weight + self.style_weight * self.loss_s+\
                     self.l_identity1 * 50 + self.l_identity2 * 1 + \

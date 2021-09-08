73efcd05f8ef56773f5be4616f7050bb94af332e
David Gill <davegill@Davids-MBP.home>
2021-09-08 05:09:55 -0400

    linearxvit

--- ppgan/models/generators/xvit_linear.py
+++ ppgan/models/generators/xvit_linear.py
@@ -392,7 +392,7 @@ class LocalAttention(nn.Layer):
             dots[~mask]= mask_value
             del mask
 
-        attn = dots.softmax(axis=-1)
+        attn = paddle.softmax(dots,axis=-1)
         attn = self.dropout(attn)
         bv = paddle.concat([v_cls,bv],axis=1)
         attn = paddle.concat([paddle.ones(bv.shape),attn],axis=1)

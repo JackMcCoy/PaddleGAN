df9962329e7554e810eedebb50c03ae842ff456b
JackMcCoy <fdrdavegill@gmail.com>
2021-09-09 14:27:02 -0400

    increase style remd with increased style weights

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -330,10 +330,10 @@ class LapStyleDraXDOG(BaseModel):
         mxdog_weight = .5
 
 
-        self.loss = self.loss_c * self.content_weight + self.style_weight * self.loss_s+\
+        self.loss = self.loss_c * self.content_weight + self.style_weight * (self.loss_s+self.loss_style_remd*10)+\
                     self.l_identity1 * 50 + self.l_identity2 * 1 + \
                     mxdog_losses*mxdog_weight+\
-                    self.loss_content_relt * 16 + self.loss_style_remd*10
+                    self.loss_content_relt * 16
 
         return self.loss
 

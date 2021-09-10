fa888fc0774449644fa6ed5103f669af6924615e
JackMcCoy <fdrdavegill@gmail.com>
2021-09-10 00:57:44 -0400

    dual ci, concat tensors

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -299,9 +299,9 @@ class LapStyleDraXDOG(BaseModel):
             self.loss_s += self.calc_style_loss(self.tF[layer], self.sF[layer])
         self.losses['loss_s'] = self.loss_s
         """IDENTITY LOSSES"""
-        dual_ci = paddle.concat(x=[self.ci, self.ci], axis=1)
-        self.Icc = self.nets['net_vit'](dual_ci)
-        self.l_identity1 = self.calc_content_loss(self.Icc, self.ci)
+        dual_si = paddle.concat(x=[self.si, self.si], axis=1)
+        self.Icc = self.nets['net_vit'](dual_si)
+        self.l_identity1 = self.calc_content_loss(self.Icc, self.si)
         self.Fcc = self.nets['net_enc'](self.Icc)
         self.l_identity2 = 0
         for layer in self.content_layers:

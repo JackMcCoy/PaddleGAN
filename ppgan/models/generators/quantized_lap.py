e862b3ea482123b0275fd9aea6642785dc1d55a8
JackMcCoy <fdrdavegill@gmail.com>
2021-09-10 22:51:43 -0400

    vector quantize (check vgg sizes)

--- ppgan/models/generators/quantized_lap.py
+++ ppgan/models/generators/quantized_lap.py
@@ -43,7 +43,7 @@ class VectorQuantize(nn.Layer):
 
         embed = paddle.randn((dim, n_embed))
         self.register_buffer('embed', embed)
-        self.register_buffer('cluster_size', paddle.zeros(shape=(n_embed)))
+        self.register_buffer('cluster_size', paddle.zeros(shape=(n_embed,1)))
         self.register_buffer('embed_avg', embed.clone())
 
     @property

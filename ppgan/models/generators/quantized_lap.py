8dfa13cf5c40421e503fdd01e820236ff3a541ac
JackMcCoy <fdrdavegill@gmail.com>
2021-09-10 23:04:30 -0400

    vector quantize (check vgg sizes)

--- ppgan/models/generators/quantized_lap.py
+++ ppgan/models/generators/quantized_lap.py
@@ -57,6 +57,7 @@ class VectorQuantize(nn.Layer):
             - 2 * flatten @ self.embed
             + self.embed.pow(2).sum(0, keepdim=True)
         )
+        print(dist.shape)
         print((-dist).max(1))
         embed_ind = (-dist).max(1)
         embed_onehot = F.one_hot(embed_ind, self.n_embed)

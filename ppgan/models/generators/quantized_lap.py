f24384388a65bfeef2c8889f80e198002a883d79
JackMcCoy <fdrdavegill@gmail.com>
2021-09-11 04:41:02 -0400

    quantized lapstyle

--- ppgan/models/generators/quantized_lap.py
+++ ppgan/models/generators/quantized_lap.py
@@ -62,7 +62,7 @@ class VectorQuantize(nn.Layer):
         quantize = F.embedding(embed_ind, self.embed.transpose((1,0)))
         if self.training:
             ema_inplace(self.cluster_size, embed_onehot.sum(0), self.decay)
-            embed_sum = flatten.transpose((1,0)) @ embed_onehot
+            embed_sum = paddle.matmul(flatten.transpose((1,0)), embed_onehot)
             ema_inplace(self.embed_avg, embed_sum, self.decay)
             cluster_size = laplace_smoothing(self.cluster_size, self.n_embed, self.eps) * self.cluster_size.sum()
             embed_normalized = self.embed_avg / cluster_size.unsqueeze(axis=0)

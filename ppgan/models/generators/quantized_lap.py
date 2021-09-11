c32c066013856290e981157c20e41b8497e5c730
JackMcCoy <fdrdavegill@gmail.com>
2021-09-11 01:05:17 -0400

    new linear transformer params

--- ppgan/models/generators/quantized_lap.py
+++ ppgan/models/generators/quantized_lap.py
@@ -85,7 +85,7 @@ class DecoderQuantized(nn.Layer):
     def __init__(self):
         super(DecoderQuantized, self).__init__()
 
-        self.quantize_4 = VectorQuantize(256, 1024)
+        self.quantize_4 = VectorQuantize(128, 1024)
         self.quantize_3 = VectorQuantize(1024, 1024)
         self.quantize_2 = VectorQuantize(1024, 1024)
 

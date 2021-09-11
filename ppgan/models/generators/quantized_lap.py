d3fd827d093942569dd98a255997e094fabe1a17
JackMcCoy <fdrdavegill@gmail.com>
2021-09-11 06:58:17 -0400

    get onehot dims

--- ppgan/models/generators/quantized_lap.py
+++ ppgan/models/generators/quantized_lap.py
@@ -49,7 +49,7 @@ class VectorQuantize(nn.Layer):
         if n_embed != 1280:
             self.rearrange = Rearrange('b c h w -> b (h w) c')
         else:
-            self.rearrange = Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)',p1=16,p2=16)
+            self.rearrange = Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)',p1=4,p2=4)
         self.decompose_axis = Rearrange('b (h w) c -> b c h w',h=dim)
 
     @property

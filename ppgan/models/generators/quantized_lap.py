57722890d9a99ab00f0740a994bb78e8f78166b4
JackMcCoy <fdrdavegill@gmail.com>
2021-09-11 07:40:23 -0400

    rearrange

--- ppgan/models/generators/quantized_lap.py
+++ ppgan/models/generators/quantized_lap.py
@@ -50,7 +50,7 @@ class VectorQuantize(nn.Layer):
             self.rearrange = Rearrange('b c h w -> b (h w) c')
             self.decompose_axis = Rearrange('b (h w) c -> b c h w',h=dim)
             self.transformer = Transformer(n_embed, 2, 4, n_embed, n_embed)
-            self.pos_embedding = paddle.create_parameter(shape=(1, n_embed, dim**2), dtype='float32')
+            self.pos_embedding = paddle.create_parameter(shape=(1, n_embed, dim**2*2), dtype='float32')
         else:
             self.rearrange = Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)',p1=4,p2=4)
             self.decompose_axis = Rearrange('b (h w) (e d c) -> b c (h e) (w d)',h=16,w=16, e=4,d=4)

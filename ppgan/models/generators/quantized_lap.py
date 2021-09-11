b85e8a63159ba0c827913b852094849dfb902332
JackMcCoy <fdrdavegill@gmail.com>
2021-09-11 02:03:43 -0400

    new linear transformer params

--- ppgan/models/generators/quantized_lap.py
+++ ppgan/models/generators/quantized_lap.py
@@ -87,7 +87,7 @@ class DecoderQuantized(nn.Layer):
 
         self.quantize_4 = VectorQuantize(16, 320)
         self.quantize_3 = VectorQuantize(16, 320)
-        self.quantize_2 = VectorQuantize(16, 640)
+        self.quantize_2 = VectorQuantize(16, 320)
 
         self.resblock_41 = ResnetBlock(512)
         self.convblock_41 = ConvBlock(512, 256)

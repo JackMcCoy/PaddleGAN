e131c4d2221cc6336d538a2948c3993eeb6ee700
JackMcCoy <fdrdavegill@gmail.com>
2021-09-11 08:02:24 -0400

    rearrange

--- ppgan/models/generators/quantized_lap.py
+++ ppgan/models/generators/quantized_lap.py
@@ -91,6 +91,7 @@ class VectorQuantize(nn.Layer):
         loss = F.mse_loss(quantize.detach(), input) * self.commitment
         quantize = input + (quantize - input).detach()
         quantize = self.rearrange(quantize)
+        print(quantize.shape)
         b, n, _ = quantize.shape
         quantize += self.pos_embedding[:, :n]
         quantize = self.transformer(quantize)

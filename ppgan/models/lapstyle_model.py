d5777e250ead588478084343acec381384abebba
JackMcCoy <fdrdavegill@gmail.com>
2021-09-12 06:09:51 -0400

    deeper transformers

--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -304,12 +304,8 @@ class LapStyleDraXDOG(BaseModel):
         self.sX,_ = xdog(self.si.detach(),self.gaussian_filter,self.gaussian_filter_2,self.morph_conv,gamma=self.gamma,morph_cutoff=self.morph_cutoff,morphs=1)
         self.cXF = self.nets['net_enc'](self.cX)
         self.sXF = self.nets['net_enc'](self.sX)
-        stylized_dog,_ = xdog(self.stylized,self.gaussian_filter,self.gaussian_filter_2,self.morph_conv,gamma=.85,morph_cutoff=7,morphs=1)
+        stylized_dog,_ = xdog(self.stylized,self.gaussian_filter,self.gaussian_filter_2,self.morph_conv,gamma=self.gamma,morph_cutoff=self.morph_cutoff,morphs=1)
         self.cdogF = self.nets['net_enc'](stylized_dog)
-
-        self.visual_items['cX'] = self.cX
-        self.visual_items['sX'] = self.sX
-        self.visual_items['stylized_dog'] = stylized_dog
 
         self.tF = self.nets['net_enc'](self.stylized)
         """content loss"""
@@ -357,7 +353,10 @@ class LapStyleDraXDOG(BaseModel):
 
         mxdog_content = self.calc_content_loss(self.tF['r31'], self.cXF['r31'])+self.calc_content_loss(self.tF['r41'], self.cXF['r41'])
         mxdog_content_contraint = self.calc_content_loss(self.cdogF['r31'], self.cXF['r31'])+self.calc_content_loss(self.cdogF['r41'], self.cXF['r41'])
-        mxdog_content_img = self.mse_loss(self.cdogF['r31'],self.sXF['r31']) + self.mse_loss(self.cdogF['r41'],self.sXF['r41'])
+        if self.steps>1200:
+            mxdog_content_img = self.mse_loss(self.cdogF['r31'],self.sXF['r31']) + self.mse_loss(self.cdogF['r41'],self.sXF['r41'])
+        else:
+            mxdog_content_img = 0
 
         self.losses['loss_MD'] = mxdog_content*.3
         self.losses['loss_CnsC'] = mxdog_content_contraint*100

0540f0629c6705c31d95b665ecbdbf0e5bc3a39b
JackMcCoy <fdrdavegill@gmail.com>
2021-09-12 19:38:20 -0400

    linear transformer

--- ppgan/models/generators/quantized_lap.py
+++ ppgan/models/generators/quantized_lap.py
@@ -104,13 +104,16 @@ class VectorQuantize(nn.Layer):
             self.decompose_axis = Rearrange('b (h w) (e d c) -> b c (h e) (w d)',h=16,w=16, e=4,d=4)
 
         if transformer_size==1:
-            self.transformer = Transformer(dim**2*2, 6, 8, dim**2*2, dim**2*2, dropout=0.1)
+            self.transformer = ImageLinearAttention(512, kernel_size = 1, padding = 0, stride = 1, key_dim = 16, value_dim = 16, heads = 8, norm_queries = False)
+            #self.transformer = Transformer(dim**2*2, 6, 8, dim**2*2, dim**2*2, dropout=0.1)
             self.pos_embedding = paddle.create_parameter(shape=(1, 256, 512), dtype='float32')
         elif transformer_size==2:
-            self.transformer = Transformer(256, 4, 8, 256, 256, dropout=0.1)
+            self.transformer = ImageLinearAttention(256, kernel_size = 1, padding = 0, stride = 1, key_dim = 32, value_dim = 32, heads = 8, norm_queries = False)
+            #self.transformer = Transformer(256, 4, 8, 256, 256, dropout=0.1)
             self.pos_embedding = paddle.create_parameter(shape=(1, 1024, 256), dtype='float32')
         elif transformer_size==3:
-            self.transformer = Transformer(2048, 2, 8, 1024, 2048, dropout=0.1)
+            self.transformer = ImageLinearAttention(128, kernel_size = 1, padding = 0, stride = 1, key_dim = 64, value_dim = 64, heads = 8, norm_queries = False)
+            #self.transformer = Transformer(2048, 2, 8, 1024, 2048, dropout=0.1)
             self.pos_embedding = paddle.create_parameter(shape=(1, 256, 2048), dtype='float32')
     @property
     def codebook(self):
@@ -138,12 +141,7 @@ class VectorQuantize(nn.Layer):
 
 
         loss = F.mse_loss(quantize.detach(), input) * self.commitment
-        print(quantize.shape)
-        quantize = self.rearrange(quantize)
-        b, n, _ = quantize.shape
-        quantize += self.pos_embedding[:, :n]
         quantize = self.transformer(quantize)
-        quantize = self.decompose_axis(quantize)
         quantize = input + (quantize - input).detach()
         return quantize, embed_ind, loss
 

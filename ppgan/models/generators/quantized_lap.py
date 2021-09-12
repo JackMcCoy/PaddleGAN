46897e6960a53be5ed145a1d1462f055da83a833
JackMcCoy <fdrdavegill@gmail.com>
2021-09-12 19:26:15 -0400

    print shape of quantized

--- ppgan/models/generators/quantized_lap.py
+++ ppgan/models/generators/quantized_lap.py
@@ -23,6 +23,54 @@ def ema_inplace(moving_avg, new, decay):
 def laplace_smoothing(x, n_categories, eps=1e-5):
     return (x + eps) / (x.sum() + n_categories * eps)
 
+class ImageLinearAttention(nn.Layer):
+    def __init__(self, chan, chan_out = None, kernel_size = 1, padding = 0, stride = 1, key_dim = 64, value_dim = 64, heads = 8, norm_queries = True):
+        super().__init__()
+        self.chan = chan
+        chan_out = chan if chan_out is None else chan_out
+
+        self.key_dim = key_dim
+        self.value_dim = value_dim
+        self.heads = heads
+
+        self.norm_queries = norm_queries
+
+        conv_kwargs = {'padding': padding, 'stride': stride}
+        self.to_q = nn.Conv2d(chan, key_dim * heads, kernel_size, **conv_kwargs)
+        self.to_k = nn.Conv2d(chan, key_dim * heads, kernel_size, **conv_kwargs)
+        self.to_v = nn.Conv2d(chan, value_dim * heads, kernel_size, **conv_kwargs)
+
+        out_conv_kwargs = {'padding': padding}
+        self.to_out = nn.Conv2d(value_dim * heads, chan_out, kernel_size, **out_conv_kwargs)
+
+    def forward(self, x, context = None):
+        b, c, h, w, k_dim, heads = *x.shape, self.key_dim, self.heads
+
+        q, k, v = (self.to_q(x), self.to_k(x), self.to_v(x))
+
+        q, k, v = map(lambda t: t.reshape((b, heads, -1, h * w)), (q, k, v))
+
+        q, k = map(lambda x: x * (self.key_dim ** -0.25), (q, k))
+
+        if context is not None:
+            context = context.reshape((b, c, 1, -1))
+            ck, cv = self.to_k(context), self.to_v(context)
+            ck, cv = map(lambda t: t.reshape((b, heads, k_dim, -1)), (ck, cv))
+            k = paddle.concat((k, ck), axis=3)
+            v = paddle.concat((v, cv), axis=3)
+
+        k = k.softmax(axis=-1)
+
+        if self.norm_queries:
+            q = q.softmax(axis=-2)
+
+        context = paddle.matmul(k, v.transpose([0,1,3,2]))
+        out = paddle.matmul(q, context)
+        #out = torch.einsum('bhdn,bhde->bhen', q, context)
+        out = out.reshape((b, -1, h, w))
+        out = self.to_out(out)
+        return out
+
 class VectorQuantize(nn.Layer):
     def __init__(
         self,
@@ -90,6 +138,7 @@ class VectorQuantize(nn.Layer):
 
 
         loss = F.mse_loss(quantize.detach(), input) * self.commitment
+        print(quantize.shape)
         quantize = self.rearrange(quantize)
         b, n, _ = quantize.shape
         quantize += self.pos_embedding[:, :n]

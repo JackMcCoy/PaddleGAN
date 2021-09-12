10da4adc3e95b8f2a7cc7253d600499b3d5a5bd4
JackMcCoy <fdrdavegill@gmail.com>
2021-09-12 06:48:26 -0400

    learn codebook loss weights

--- ppgan/models/generators/quantized_lap.py
+++ ppgan/models/generators/quantized_lap.py
@@ -57,13 +57,13 @@ class VectorQuantize(nn.Layer):
 
         if transformer_size==1:
             self.transformer = Transformer(dim**2*2, 6, 8, 1024, dim**2*2, dropout=0.1)
-            self.pos_embedding = paddle.create_parameter(shape=(1, 256, 512), dtype='float32', is_bias=True)
+            self.pos_embedding = paddle.create_parameter(shape=(1, 256, 512), dtype='float32')
         elif transformer_size==2:
             self.transformer = Transformer(256, 4, 8, 256, 256, dropout=0.1)
-            self.pos_embedding = paddle.create_parameter(shape=(1, 1024, 256), dtype='float32', is_bias=True)
+            self.pos_embedding = paddle.create_parameter(shape=(1, 1024, 256), dtype='float32')
         elif transformer_size==3:
             self.transformer = Transformer(2048, 2, 8, 2048, 2048, dropout=0.1)
-            self.pos_embedding = paddle.create_parameter(shape=(1, 256, 2048), dtype='float32', is_bias=True)
+            self.pos_embedding = paddle.create_parameter(shape=(1, 256, 2048), dtype='float32')
     @property
     def codebook(self):
         return self.embed.transpose([1, 0])
--- ppgan/models/lapstyle_model.py
+++ ppgan/models/lapstyle_model.py
@@ -367,9 +367,9 @@ class LapStyleDraXDOG(BaseModel):
         self.losses['book_weight2'] = self.nets['net_vit'].book_weight2
         self.losses['book_weight3'] = self.nets['net_vit'].book_weight3
 
-        self.losses['map_loss'] = self.map_loss * self.nets['net_vit'].book_weight
-        self.losses['ci_book_loss'] = book_loss * self.nets['net_vit'].book_weight2
-        self.losses['si_book_loss'] = book_loss_s * self.nets['net_vit'].book_weight3
+        self.losses['map_loss'] = self.map_loss
+        self.losses['ci_book_loss'] = book_loss
+        self.losses['si_book_loss'] = book_loss_s
 
         pred_fake = self.nets['netD'](self.stylized)
         self.loss_G_GAN = self.gan_criterion(pred_fake, True)

a3a8dec5d9cebae2d74826e0d1613a5e4c9447a4
JackMcCoy <fdrdavegill@gmail.com>
2021-09-12 15:17:14 -0400

    mdog style loss

--- ppgan/models/criterions/pixel_loss.py
+++ ppgan/models/criterions/pixel_loss.py
@@ -260,6 +260,6 @@ class GramErrors():
             pred (Tensor): of shape (N, C, H, W). Predicted tensor.
             target (Tensor): of shape (N, C, H, W). Ground truth tensor.
         """
-        gram_pred = self.gram_matrix(pred)
-        gram_target = self.gram_matrix(target)
-        return self.mse_loss(gram_pred, gram_target)
+        gram_pred = paddle.clip(self.gram_matrix(pred))
+        gram_target = paddle.clip(self.gram_matrix(target))
+        return paddle.clip(self.mse_loss(gram_pred, gram_target))

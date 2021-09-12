21e21f86545455686603bb1ada7be1efb0882c1d
JackMcCoy <fdrdavegill@gmail.com>
2021-09-11 22:00:26 -0400

    log onehot dim size for transformer sizing

--- ppgan/models/generators/quantized_lap.py
+++ ppgan/models/generators/quantized_lap.py
@@ -79,11 +79,7 @@ class VectorQuantize(nn.Layer):
         embed_onehot = F.one_hot(embed_ind, self.n_embed)
         embed_ind = paddle.reshape(embed_ind,shape=(input.shape[0],input.shape[1],input.shape[2]))
         quantize = F.embedding(embed_ind, self.embed.transpose((1,0)))
-        quantize = self.rearrange(quantize)
-        b, n, _ = quantize.shape
-        quantize += self.pos_embedding[:, :n]
-        quantize = self.transformer(quantize)
-        quantize = self.decompose_axis(quantize)
+
         if self.training:
             ema_inplace(self.cluster_size, embed_onehot.sum(0), self.decay)
             embed_sum = paddle.matmul(flatten.transpose((1,0)), embed_onehot)
@@ -94,6 +90,11 @@ class VectorQuantize(nn.Layer):
 
 
         loss = F.mse_loss(quantize.detach(), input) * self.commitment
+        quantize = self.rearrange(quantize)
+        b, n, _ = quantize.shape
+        quantize += self.pos_embedding[:, :n]
+        quantize = self.transformer(quantize)
+        quantize = self.decompose_axis(quantize)
         quantize = input + (quantize - input).detach()
         return quantize, embed_ind, loss
 

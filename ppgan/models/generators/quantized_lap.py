42e1d94f4e2cdbc2d4e5c1a7c3f3b0fe2112ac34
JackMcCoy <fdrdavegill@gmail.com>
2021-09-12 20:41:35 -0400

    add GELU

--- ppgan/models/generators/quantized_lap.py
+++ ppgan/models/generators/quantized_lap.py
@@ -441,15 +441,15 @@ class VectorQuantize(nn.Layer):
         if transformer_size==1:
             self.transformer = ImageLinearAttention(512, kernel_size = 1, padding = 0, stride = 1, key_dim = 16, value_dim = 16, heads = 8, norm_queries = True)
             #self.transformer = Transformer(dim**2*2, 6, 8, dim**2*2, dim**2*2, dropout=0.1)
-            self.pos_embedding = paddle.create_parameter(shape=(1, 256, 512), dtype='float32')
+            self.pos_embedding = paddle.create_parameter(shape=(1, 512, 16, 16), dtype='float32')
         elif transformer_size==2:
             self.transformer = ImageLinearAttention(256, kernel_size = 1, padding = 0, stride = 1, key_dim = 32, value_dim = 32, heads = 8, norm_queries = True)
             #self.transformer = Transformer(256, 4, 8, 256, 256, dropout=0.1)
-            self.pos_embedding = paddle.create_parameter(shape=(1, 1024, 256), dtype='float32')
+            self.pos_embedding = paddle.create_parameter(shape=(1, 256, 32, 32), dtype='float32')
         elif transformer_size==3:
             self.transformer = ImageLinearAttention(128, kernel_size = 1, padding = 0, stride = 1, key_dim = 64, value_dim = 64, heads = 8, norm_queries = True)
             #self.transformer = Transformer(2048, 2, 8, 1024, 2048, dropout=0.1)
-            self.pos_embedding = paddle.create_parameter(shape=(1, 256, 2048), dtype='float32')
+            self.pos_embedding = paddle.create_parameter(shape=(1, 128, 64, 64), dtype='float32')
     @property
     def codebook(self):
         return self.embed.transpose([1, 0])
@@ -476,6 +476,7 @@ class VectorQuantize(nn.Layer):
 
 
         loss = F.mse_loss(quantize.detach(), input) * self.commitment
+        print(quantize.shape)
         quantize = self.transformer(self.pos_embedding+quantize)
         quantize = input + (quantize - input).detach()
         return quantize, embed_ind, loss

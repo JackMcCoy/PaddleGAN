d902bed9ce492c625768356874fad81611d7d516
JackMcCoy <fdrdavegill@gmail.com>
2021-09-17 03:41:47 -0400

    replace book loss

--- ppgan/models/generators/quantized_lap.py
+++ ppgan/models/generators/quantized_lap.py
@@ -373,7 +373,8 @@ class VectorQuantize(nn.Layer):
             self.rearrange = Rearrange('b c h w -> b (h w) c')
             if transformer_size==4:
                 self.decompose_axis = Rearrange('b (h w) (e d c) -> b c (h e) (w d)',h=32,w=32, e=4,d=4)
-            self.decompose_axis = Rearrange('b (h w) c -> b c h w',h=dim)
+            else:
+                self.decompose_axis = Rearrange('b (h w) c -> b c h w',h=dim)
         else:
             self.rearrange = Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)',p1=4,p2=4)
             self.decompose_axis = Rearrange('b (h w) (e d c) -> b c (h e) (w d)',h=16,w=16, e=4,d=4)

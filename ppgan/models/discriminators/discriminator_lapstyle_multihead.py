9196dd9b41b493c06123e1cc2cf2b0fcea9ef8e5
JackMcCoy <fdrdavegill@gmail.com>
2021-09-16 20:58:11 -0400

    quantizer/transformer in discriminator

--- ppgan/models/discriminators/discriminator_lapstyle_multihead.py
+++ ppgan/models/discriminators/discriminator_lapstyle_multihead.py
@@ -191,8 +191,8 @@ class VectorDiscQuantize(nn.Layer):
             self.transformer = Transformer(256, 8, 16, 64, 256, dropout=0.1)
             self.pos_embedding = nn.Embedding(1024, 256)
         elif transformer_size==3:
-            self.transformer = Transformer(2048, 8, 16, 64, 768, dropout=0.1)
-            self.pos_embedding = nn.Embedding(256, 2048)
+            self.transformer = Transformer(1024, 8, 16, 64, 768, dropout=0.1)
+            self.pos_embedding = nn.Embedding(256, 1024)
     @property
     def codebook(self):
         return self.embed.transpose([1, 0])

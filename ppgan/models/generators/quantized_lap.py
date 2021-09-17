3af66c7f76d843050258853e8dde9987c6cc3fb2
JackMcCoy <fdrdavegill@gmail.com>
2021-09-17 04:03:23 -0400

    replace book loss

--- ppgan/models/generators/quantized_lap.py
+++ ppgan/models/generators/quantized_lap.py
@@ -391,7 +391,7 @@ class VectorQuantize(nn.Layer):
             self.pos_embedding = nn.Embedding(256, 2048)
         elif transformer_size==4:
             self.transformer = Transformer(64, 4, 16, 64, 128, dropout=0.1)
-            self.pos_embedding = nn.Embedding(256, 64)
+            self.pos_embedding = nn.Embedding(256, 128)
     @property
     def codebook(self):
         return self.embed.transpose([1, 0])

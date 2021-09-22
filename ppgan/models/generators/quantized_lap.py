58f8ef783ede6adde79a741b113fee25c290ffde
JackMcCoy <fdrdavegill@gmail.com>
2021-09-22 01:58:16 -0400

    transformer at end

--- ppgan/models/generators/quantized_lap.py
+++ ppgan/models/generators/quantized_lap.py
@@ -531,7 +531,6 @@ class DecoderQuantized(nn.Layer):
         position_ids.stop_gradient = True
         position_embeddings = self.pos_embedding(position_ids)
         transformer = self.rearrange(out)
-        print(transformer.shape)
         transformer = transformer + position_embeddings
         transformer = self.vit(transformer)
         transformer = self.decompose_axis(transformer)
